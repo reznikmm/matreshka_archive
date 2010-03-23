35db1765f525e23dd8abe086ab7a6e47e231e149
Vadim Godunko <vgodunko@gmail.com>
2010-03-23 01:09:10 +0000

    Switch to Wide_Wide_Character.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@320 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- design/xml/tools/aflex/src/scanner-dfa.adb
+++ design/xml/tools/aflex/src/scanner-dfa.adb
@@ -1,41 +1,43 @@
 package body scanner.DFA is
-function YYText return Wide_Wide_String is
-   Aux : constant Wide_Wide_String (1 .. yy_cp - yy_bp)
-     := Wide_Wide_String (yy_ch_buf.data (yy_bp .. yy_cp - 1));
 
-begin
-   return Aux;
-end;
+   function YYText return Wide_Wide_String is
+      Aux : constant Wide_Wide_String (1 .. YY_CP - YY_BP)
+        := Wide_Wide_String (YY_Ch_Buf.Data (YY_BP .. YY_CP - 1));
 
--- returns the length of the matched text
-function YYLength return integer is
-begin
-    return yy_cp - yy_bp;
-end YYLength;
+   begin
+      return Aux;
+   end YYText;
 
--- done after the current pattern has been matched and before the
--- corresponding action - sets up yytext
+   --  returns the length of the matched text
+   function YYLength return Integer is
+   begin
+      return YY_CP - YY_BP;
+   end YYLength;
 
-procedure YY_DO_BEFORE_ACTION is
-begin
-    yytext_ptr := yy_bp;
-    yy_c_buf_p := yy_cp;
-end YY_DO_BEFORE_ACTION;
+   --  done after the current pattern has been matched and before the
+   --  corresponding action - sets up yytext
 
-function Previous (Data : ch_buf_type; Index : Integer) return Wide_Wide_Character is
-   Aux : constant Integer := Index - 1;
+   procedure YY_DO_BEFORE_ACTION is
+   begin
+      YYText_Ptr := YY_BP;
+      YY_C_Buf_P := YY_CP;
+   end YY_DO_BEFORE_ACTION;
 
-begin
-   return Data.Data (Aux);
-end Previous;
+   function Previous
+     (Data : CH_Buf_Type; Index : Integer) return Wide_Wide_Character
+   is
+      Aux : constant Integer := Index - 1;
 
-procedure Next
-  (Data  : ch_buf_type;
-   Index : in out Integer;
-   Code  : out Wide_Wide_Character)
-is
-begin
-   Code := Data.Data (Index);
-   Index := Index + 1;
-end Next;
+   begin
+      return Data.Data (Aux);
+   end Previous;
+
+   procedure Next
+     (Data  : CH_Buf_Type;
+      Index : in out Integer;
+      Code  : out Wide_Wide_Character) is
+   begin
+      Code := Data.Data (Index);
+      Index := Index + 1;
+   end Next;
 end scanner.DFA;
--- design/xml/tools/aflex/src/scanner-dfa.ads
+++ design/xml/tools/aflex/src/scanner-dfa.ads
@@ -1,33 +1,45 @@
 package scanner.DFA is
 Aflex_Debug : Boolean := False;
-yytext_ptr : integer; -- points to start of yytext in buffer
 
--- yy_ch_buf has to be 2 characters longer than YY_BUF_SIZE because we need
--- to put in 2 end-of-buffer characters (this is explained where it is
--- done) at the end of yy_ch_buf
-YY_READ_BUF_SIZE : constant integer :=  8192;
-YY_BUF_SIZE : constant integer := YY_READ_BUF_SIZE * 2; -- size of input buffer
-type unbounded_character_array is array(integer range <>) of wide_wide_character;
-type ch_buf_type is record
-   Data : unbounded_character_array(0..YY_BUF_SIZE + 1);
-end record;
-function Previous (Data : ch_buf_type; Index : Integer) return Wide_Wide_Character;
-procedure Next
-  (Data  : ch_buf_type;
-   Index : in out Integer;
-   Code  : out Wide_Wide_Character);
-yy_ch_buf : ch_buf_type;
-yy_cp, yy_bp : integer;
+   YYText_Ptr : Integer; -- points to start of yytext in buffer
 
-yy_c_buf_p : integer;   -- points to current character in buffer
+   --  yy_ch_buf has to be 2 characters longer than YY_BUF_SIZE because we
+   --  need to put in 2 end-of-buffer characters (this is explained where
+   --  it is done) at the end of yy_ch_buf
+   YY_READ_BUF_SIZE : constant Integer := 8192;
+   YY_BUF_SIZE      : constant Integer := YY_READ_BUF_SIZE * 2;
+   --  Size of input buffer
 
-function YYText return Wide_Wide_string;
-function YYLength return integer;
-procedure YY_DO_BEFORE_ACTION;
---These variables are needed between calls to YYLex.
-yy_init : boolean := true; -- do we need to initialize YYLex?
-yy_start : integer := 0; -- current start state number
-subtype YY_State_Type is integer;
-yy_last_accepting_state : yy_state_type;
-yy_last_accepting_cpos : integer;
+   type Unbounded_Character_Array is
+     array (Integer range <>) of Wide_Wide_Character;
+
+   type Ch_Buf_Type is record
+      Data : Unbounded_Character_Array (0 .. YY_BUF_SIZE + 1);
+   end record;
+
+   function Previous
+     (Data : Ch_Buf_Type; Index : Integer) return Wide_Wide_Character;
+   procedure Next
+     (Data  : Ch_Buf_Type;
+      Index : in out Integer;
+      Code  : out Wide_Wide_Character);
+
+   YY_Ch_Buf : Ch_Buf_Type;
+   YY_CP     : Integer;
+   YY_BP     : Integer;
+
+   YY_C_Buf_P : Integer;  --  Points to current character in buffer
+
+   function YYText return Wide_Wide_String;
+   function YYLength return Integer;
+   procedure YY_DO_BEFORE_ACTION;
+
+   --  These variables are needed between calls to YYLex.
+
+   YY_Init  : Boolean := True;  --  do we need to initialize YYLex?
+   YY_Start : Integer := 0;     -- current start state number
+   subtype YY_State_Type is Integer;
+   YY_Last_Accepting_State : YY_State_Type;
+   YY_Last_Accepting_Cpos  : Integer;
+
 end scanner.DFA;
