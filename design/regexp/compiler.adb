55d91a8d7d90d28ae7041bfb4b8f04558680889d
Vadim Godunko <vgodunko@gmail.com>
2010-03-23 09:39:45 +0000

    Rewrite compiler to produce more compact programs and be more extendable.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@328 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- design/regexp/compiler.adb
+++ design/regexp/compiler.adb
@@ -1,75 +1,114 @@
+with Ada.Containers.Vectors;
 
 package body Compiler is
 
    use Engine;
    use Syntax;
 
+   package Integer_Vectors is
+     new Ada.Containers.Vectors (Positive, Integer);
+
    -------------
    -- Compile --
    -------------
 
    function Compile return Engine.Instruction_Array is
-      Program : Instruction_Array (1 .. AST_Last * 3);
-      Last    : Natural := 1;
+      use Integer_Vectors;
 
-      function Compile (Expression : Positive) return Positive;
+      Program : Instruction_Array (1 .. AST_Last * 3);
+      Last    : Natural := 0;
+
+      procedure Compile
+        (Expression  : Positive;
+         Instruction : out Positive;
+         Tails       : out Vector);
 
       -------------
       -- Compile --
       -------------
 
-      function Compile (Expression : Positive) return Positive is
-         Ins : Positive;
-
+      procedure Compile
+        (Expression  : Positive;
+         Instruction : out Positive;
+         Tails       : out Vector) is
       begin
          Last := Last + 1;
-         Ins  := Last;
+         Instruction := Last;
+         Tails.Clear;
 
          case AST (Expression).Kind is
-            when None =>
-               null;
-
             when Alternation =>
-               Program (Ins) :=
-                 (Split,
-                  Compile (AST (Expression).First),
-                  Compile (AST (Expression).Second));
+               declare
+                  Ins_1   : Positive;
+                  Ins_2   : Positive;
+                  Tails_1 : Vector;
+                  Tails_2 : Vector;
+
+               begin
+                  Compile (AST (Expression).First, Ins_1, Tails_1);
+                  Compile (AST (Expression).Second, Ins_2, Tails_2);
+                  Program (Instruction) := (Split, Ins_1, Ins_2);
+
+                  Tails := Tails_1 & Tails_2;
+               end;
 
             when Any_Code_Point =>
-               Program (Ins) := (Any_Code_Point, 0);
+               Program (Instruction) := (Any_Code_Point, 0);
 
                if AST (Expression).Next = 0 then
-                  Last := Last + 1;
-                  Program (Last) := (Kind => Match);
-                  Program (Ins).Next := Last;
+                  Tails.Append (Instruction);
 
                else
-                  Program (Ins).Next := Compile (AST (Expression).Next);
+                  declare
+                     Ins : Positive;
+
+                  begin
+                     Compile (AST (Expression).Next, Ins, Tails);
+                     Program (Instruction).Next := Ins;
+                  end;
                end if;
 
             when Code_Point =>
-               Program (Ins) := (Code_Point, 0, AST (Expression).Code);
+               Program (Instruction) := (Code_Point, 0, AST (Expression).Code);
 
                if AST (Expression).Next = 0 then
-                  Last := Last + 1;
-                  Program (Last) := (Kind => Match);
-                  Program (Ins).Next := Last;
+                  Tails.Append (Instruction);
 
                else
-                  Program (Ins).Next := Compile (AST (Expression).Next);
+                  declare
+                     Ins : Positive;
+
+                  begin
+                     Compile (AST (Expression).Next, Ins, Tails);
+                     Program (Instruction).Next := Ins;
+                  end;
                end if;
 
             when others =>
                raise Program_Error;
          end case;
-
-         return Ins;
       end Compile;
 
-      Ins : Natural := Last;
+      Ins      : Positive;
+      Tails    : Vector;
+      Position : Cursor;
 
    begin
-      Program (Ins) := (Jump, Compile (AST_Start));
+      Compile (AST_Start, Ins, Tails);
+
+      if Ins /= 1 then
+         raise Program_Error;
+      end if;
+
+      Last := Last + 1;
+      Program (Last) := (Kind => Match);
+
+      Position := Tails.First;
+
+      while Has_Element (Position) loop
+         Program (Element (Position)).Next := Last;
+         Next (Position);
+      end loop;
 
       return Program (1 .. Last);
    end Compile;
