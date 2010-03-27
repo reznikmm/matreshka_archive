2c9abf16981fddd433d2e344e1ae3508c335a961
Vadim Godunko <vgodunko@gmail.com>
2010-03-27 17:33:32 +0000

    Get rid of package Parser_Actions.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@379 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- design/regexp/matreshka-internals-regexps-compiler-parser.adb
+++ design/regexp/matreshka-internals-regexps-compiler-parser.adb
@@ -1,4 +1,3 @@
-with Parser_Actions;
 with Parser_Tokens;
 with Matreshka.Internals.Regexps.Compiler.Parser.Tables;
 with Matreshka.Internals.Regexps.Compiler.Scanner;
@@ -8,9 +7,162 @@ package body Matreshka.Internals.Regexps.Compiler.Parser is
 
    use Matreshka.Internals.Regexps.Compiler.Parser.Tables;
    use Matreshka.Internals.Regexps.Compiler.Scanner;
-   use Parser_Actions;
    use Parser_Tokens;
    use Syntax;
+
+   function Process_Alternation
+     (Alternative_1 : Positive;
+      Alternative_2 : Positive) return Positive;
+
+   function Process_Multiplicity
+     (Expression : Positive;
+      Lower      : Natural;
+      Upper      : Natural;
+      Greedy     : Boolean) return Positive;
+
+   function Process_Any_Code_Point return Positive;
+
+   function Process_Code_Point
+     (Character : Wide_Wide_Character) return Positive;
+
+   function Process_Negate_Character_Class
+     (Class : Positive) return Positive;
+
+   function Process_Character_Class_Range
+     (Class : Positive;
+      Low   : Wide_Wide_Character;
+      High  : Wide_Wide_Character) return Positive;
+
+   function Process_Character_Class_Code_Point
+     (Class : Positive;
+      Code  : Wide_Wide_Character) return Positive;
+
+   function Process_New_Character_Class return Positive;
+
+   function Process_Subexpression
+     (Expression : Positive) return Positive;
+
+   -------------------------
+   -- Process_Alternation --
+   -------------------------
+
+   function Process_Alternation
+     (Alternative_1 : Positive;
+      Alternative_2 : Positive) return Positive is
+   begin
+      AST_Last := AST_Last + 1;
+      AST (AST_Last) := (Alternation, 0, Alternative_1, Alternative_2);
+
+      return AST_Last;
+   end Process_Alternation;
+
+   ----------------------------
+   -- Process_Any_Code_Point --
+   ----------------------------
+
+   function Process_Any_Code_Point return Positive is
+   begin
+      AST_Last := AST_Last + 1;
+      AST (AST_Last) := (Any_Code_Point, 0);
+
+      return AST_Last;
+   end Process_Any_Code_Point;
+
+   ----------------------------------------
+   -- Process_Character_Class_Code_Point --
+   ----------------------------------------
+
+   function Process_Character_Class_Code_Point
+     (Class : Positive;
+      Code  : Wide_Wide_Character) return Positive is
+   begin
+      AST_Last := AST_Last + 1;
+      AST (AST_Last) := (Code_Point, 0, Code);
+      Add (Class, AST_Last);
+
+      return Class;
+   end Process_Character_Class_Code_Point;
+
+   -----------------------------------
+   -- Process_Character_Class_Range --
+   -----------------------------------
+
+   function Process_Character_Class_Range
+     (Class : Positive;
+      Low   : Wide_Wide_Character;
+      High  : Wide_Wide_Character) return Positive is
+   begin
+      AST_Last := AST_Last + 1;
+      AST (AST_Last) := (Code_Point_Range, 0, Low, High);
+      Add (Class, AST_Last);
+
+      return Class;
+   end Process_Character_Class_Range;
+
+   ------------------------
+   -- Process_Code_Point --
+   ------------------------
+
+   function Process_Code_Point
+     (Character : Wide_Wide_Character) return Positive is
+   begin
+      AST_Last := AST_Last + 1;
+      AST (AST_Last) := (Code_Point, 0, Character);
+
+      return AST_Last;
+   end Process_Code_Point;
+
+   --------------------------
+   -- Process_Multiplicity --
+   --------------------------
+
+   function Process_Multiplicity
+     (Expression : Positive;
+      Lower      : Natural;
+      Upper      : Natural;
+      Greedy     : Boolean) return Positive is
+   begin
+      AST_Last := AST_Last + 1;
+      AST (AST_Last) := (Multiplicity, 0, Expression, Greedy, Lower, Upper);
+
+      return AST_Last;
+   end Process_Multiplicity;
+
+   ------------------------------------
+   -- Process_Negate_Character_Class --
+   ------------------------------------
+
+   function Process_Negate_Character_Class
+     (Class : Positive) return Positive is
+   begin
+      AST (Class).Negated := True;
+
+      return Class;
+   end Process_Negate_Character_Class;
+
+   ---------------------------------
+   -- Process_New_Character_Class --
+   ---------------------------------
+
+   function Process_New_Character_Class return Positive is
+   begin
+      AST_Last := AST_Last + 1;
+      AST (AST_Last) := (Character_Class, 0, False, 0);
+
+      return AST_Last;
+   end Process_New_Character_Class;
+
+   ---------------------------
+   -- Process_Subexpression --
+   ---------------------------
+
+   function Process_Subexpression (Expression : Positive) return Positive is
+   begin
+      AST_Last := AST_Last + 1;
+      AST (AST_Last) := (Subexpression, 0, Expression, 0);
+
+      return AST_Last;
+   end Process_Subexpression;
 
    -------------
    -- YYParse --
--- design/regexp/parser.y
+++ design/regexp/parser.y
@@ -246,15 +246,45 @@ character_class_content : character_class_content Token_Code_Point Token_Charact
    procedure YYParse;
 ##
 with Ada.Wide_Wide_Text_IO; 
-with Parser_Actions;
 with Syntax;
 ##
    use Ada.Wide_Wide_Text_IO;
-   use Parser_Actions;
    use Syntax;
 
    function YYLex return Token is separate;
 
    procedure YYError (S : Wide_Wide_String) is null;
+
+   function Process_Alternation
+     (Alternative_1 : Positive;
+      Alternative_2 : Positive) return Positive is separate;
+
+   function Process_Multiplicity
+     (Expression : Positive;
+      Lower      : Natural;
+      Upper      : Natural;
+      Greedy     : Boolean) return Positive is separate;
+
+   function Process_Any_Code_Point return Positive is separate;
+
+   function Process_Code_Point
+     (Character : Wide_Wide_Character) return Positive is separate;
+
+   function Process_Negate_Character_Class
+     (Class : Positive) return Positive is separate;
+
+   function Process_Character_Class_Range
+     (Class : Positive;
+      Low   : Wide_Wide_Character;
+      High  : Wide_Wide_Character) return Positive is separate;
+
+   function Process_Character_Class_Code_Point
+     (Class : Positive;
+      Code  : Wide_Wide_Character) return Positive is separate;
+
+   function Process_New_Character_Class return Positive is separate;
+
+   function Process_Subexpression
+     (Expression : Positive) return Positive is separate;
 
 ##
