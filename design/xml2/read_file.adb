83b6dc0cd147324c5fca84ebf074241e71434202
Vadim Godunko <vgodunko@gmail.com>
2010-06-23 05:41:40 +0000

    Improve reading of new line character.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@597 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- design/xml2/read_file.adb
+++ design/xml2/read_file.adb
@@ -6,20 +6,13 @@ function Read_File (Name : String) return League.Strings.Universal_String is
 
    File   : File_Type;
    Result : League.Strings.Universal_String;
-   Buffer : Wide_Wide_String (1 .. 1024);
-   Last   : Natural;
 
 begin
    Open (File, In_File, Name, "wcem=8");
 
    while not End_Of_File (File) loop
-      Get_Line (File, Buffer, Last);
-      Result.Append
-       (League.Strings.To_Universal_String (Buffer (1 .. Last)));
-
-      if End_Of_Line (File) then
-         Result.Append (Ada.Characters.Wide_Wide_Latin_1.LF);
-      end if;
+      Result.Append (League.Strings.To_Universal_String (Get_Line (File)));
+      Result.Append (Ada.Characters.Wide_Wide_Latin_1.LF);
    end loop;
 
    Close (File);
