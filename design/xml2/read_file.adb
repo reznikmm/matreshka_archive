be353fa9aaf87aedd010a778f5eb8ca9a8f40e2b
Vadim Godunko <vgodunko@gmail.com>
2010-06-28 07:39:26 +0000

    Reimplement document loading procedure.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@636 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- design/xml2/read_file.adb
+++ design/xml2/read_file.adb
@@ -1,21 +1,159 @@
-with Ada.Characters.Wide_Wide_Latin_1;
-with Ada.Wide_Wide_Text_IO;
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--         Localization, Internationalization, Globalization for Ada        --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2010, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+-- UTF-8 decoding algoriphm is based on work:                               --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright (c) 2008-2009 Bjoern Hoehrmann <bjoern@hoehrmann.de>           --
+--                                                                          --
+-- Permission is hereby granted, free of charge, to any person obtaining    --
+-- a copy of this software and associated documentation files (the          --
+-- "Software"), to deal in the Software without restriction, including      --
+-- without limitation the rights to use, copy, modify, merge, publish,      --
+-- distribute, sublicense, and/or sell copies of the Software, and to       --
+-- permit persons to whom the Software is furnished to do so, subject to    --
+-- the following conditions:                                                --
+--                                                                          --
+-- The above copyright notice and this permission notice shall be included  --
+-- in all copies or substantial portions of the Software.                   --
+--                                                                          --
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS  --
+-- OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF               --
+-- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.   --
+-- IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY     --
+-- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,     --
+-- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE        --
+-- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                   --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+with Ada.Streams.Stream_IO;
+with Interfaces;
+
+with League.Strings.Internals;
+with Matreshka.Internals.Strings.Operations;
+with Matreshka.Internals.Unicode;
 
 function Read_File (Name : String) return League.Strings.Universal_String is
-   use Ada.Wide_Wide_Text_IO;
+   use Interfaces;
+   use type Matreshka.Internals.Unicode.Code_Point;
 
-   File   : File_Type;
-   Result : League.Strings.Universal_String;
+   Class : constant array (Ada.Streams.Stream_Element) of Unsigned_8
+     := ( 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+
+          1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+          9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,
+          7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,
+          7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,
+          8,  8,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
+          2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
+         10,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  4,  3,  3,
+         11,  6,  6,  6,  5,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8);
+
+   Transition : constant array (Unsigned_8 range 0 .. 143) of Unsigned_8
+     := (
+          0,  1,  2,  3,  5,  8,  7,  1,  1,  1,  4,  6,  1,  1,  1,  1,
+          1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+          1,  0,  1,  1,  1,  1,  1,  0,  1,  0,  1,  1,  1,  1,  1,  1,
+          1,  2,  1,  1,  1,  1,  1,  2,  1,  2,  1,  1,  1,  1,  1,  1,
+          1,  1,  1,  1,  1,  1,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,
+          1,  2,  1,  1,  1,  1,  1,  1,  1,  2,  1,  1,  1,  1,  1,  1,
+          1,  1,  1,  1,  1,  1,  1,  3,  1,  3,  1,  1,  1,  1,  1,  1,
+          1,  3,  1,  1,  1,  1,  1,  3,  1,  3,  1,  1,  1,  1,  1,  1,
+          1,  3,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1);
+
+   Buffer : Ada.Streams.Stream_Element_Array (1 .. 1024);
+   Last   : Ada.Streams.Stream_Element_Offset;
+   File   : Ada.Streams.Stream_IO.File_Type;
+   Data   : Matreshka.Internals.Strings.Shared_String_Access
+     := Matreshka.Internals.Strings.Shared_Empty'Access;
+   Code   : Matreshka.Internals.Unicode.Code_Point;
+   State  : Unsigned_8 := 0;
 
 begin
-   Open (File, In_File, Name, "wcem=8");
+   Ada.Streams.Stream_IO.Open (File, Ada.Streams.Stream_IO.In_File, Name);
 
-   while not End_Of_File (File) loop
-      Result.Append (League.Strings.To_Universal_String (Get_Line (File)));
-      Result.Append (Ada.Characters.Wide_Wide_Latin_1.LF);
+   while not Ada.Streams.Stream_IO.End_Of_File (File) loop
+      Ada.Streams.Stream_IO.Read (File, Buffer, Last);
+
+      for J in Buffer'First .. Last loop
+         declare
+            C : constant Unsigned_8 := Class (Buffer (J));
+
+         begin
+            if State = 0 then
+               Code :=
+                (16#FF# / (2 ** Natural (C)))
+                   and Matreshka.Internals.Unicode.Code_Point (Buffer (J));
+
+            else
+               Code :=
+                (Matreshka.Internals.Unicode.Code_Point (Buffer (J))
+                   and 16#3F#) or (Code * (2 ** 6));
+            end if;
+
+            State := Transition (State * 16 + C);
+
+            if State = 0 then
+               Matreshka.Internals.Strings.Operations.Append (Data, Code);
+            end if;
+         end;
+      end loop;
    end loop;
 
-   Close (File);
+   Ada.Streams.Stream_IO.Close (File);
 
-   return Result;
+   if State /= 0 then
+      Matreshka.Internals.Strings.Dereference (Data);
+
+      raise Constraint_Error with "mailformed UTF-8 file";
+   end if;
+
+   return League.Strings.Internals.Wrap (Data);
 end Read_File;
