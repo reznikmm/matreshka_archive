091e02437b29eda8d3eb696ee0f7ff5580552c94
Vadim Godunko <vgodunko@gmail.com>
2010-07-03 22:44:45 +0000

    Output timing information for load and for parse steps separately.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@684 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- design/xml2/performance.adb
+++ design/xml2/performance.adb
@@ -11,11 +11,12 @@ with Read_File;
 procedure Performance is
    use type Ada.Calendar.Time;
 
-   Reader  : aliased Matreshka.SAX.Simple_Readers.SAX_Simple_Reader;
-   Handler : aliased Events_Printers.Events_Printer;
-   Text    : League.Strings.Universal_String;
-   Start   : Ada.Calendar.Time;
-
+   Reader      : aliased Matreshka.SAX.Simple_Readers.SAX_Simple_Reader;
+   Handler     : aliased Events_Printers.Events_Printer;
+   Text        : League.Strings.Universal_String;
+   Load_Start  : Ada.Calendar.Time;
+   Parse_Start : Ada.Calendar.Time;
+   Stop        : Ada.Calendar.Time;
 --   task type T;
 --
 --   task body T is
@@ -28,11 +29,21 @@ begin
 
    Reader.Set_Entity_Resolver (Handler'Unchecked_Access);
 
-   Start := Ada.Calendar.Clock;
+   Load_Start := Ada.Calendar.Clock;
    Text := Read_File (Ada.Command_Line.Argument (1));
+   Parse_Start := Ada.Calendar.Clock;
    Reader.Parse (Text);
+   Stop := Ada.Calendar.Clock;
+   Put_Line
+    ("Loading time:   "
+       & League.Strings.To_Universal_String
+          (Duration'Wide_Wide_Image (Parse_Start - Load_Start)));
+   Put_Line
+    ("Parsing time:   "
+       & League.Strings.To_Universal_String
+          (Duration'Wide_Wide_Image (Stop - Parse_Start)));
    Put_Line
     ("Processing time:"
        & League.Strings.To_Universal_String
-          (Duration'Wide_Wide_Image (Ada.Calendar.Clock - Start)));
+          (Duration'Wide_Wide_Image (Stop - Load_Start)));
 end Performance;
