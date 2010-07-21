e04d03694d5b1dc07d48823a76adf007b14279d4
Vadim Godunko <vgodunko@gmail.com>
2010-07-21 05:34:51 +0000

    Add gateway package for application-wide capabilities.
    Refs #12, #13, #14.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@744 661adc5f-87f8-0310-85d6-9a7b5f58d823

---
+++ source/league/league-application.adb
@@ -0,0 +1,140 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--         Localization, Internationalization, Globalization for Ada        --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2010, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+
+package body League.Application is
+
+   ----------------------
+   -- Application_Name --
+   ----------------------
+
+   function Application_Name return League.Strings.Universal_String is
+   begin
+      return League.Strings.Empty_String;
+   end Application_Name;
+
+   -------------------------
+   -- Application_Version --
+   -------------------------
+
+   function Application_Version return League.Strings.Universal_String is
+   begin
+      return League.Strings.Empty_String;
+   end Application_Version;
+
+   ---------------
+   -- Arguments --
+   ---------------
+
+   function Arguments return League.String_Vectors.Universal_String_Vector is
+   begin
+      return X : League.String_Vectors.Universal_String_Vector;
+   end Arguments;
+
+   -----------------
+   -- Environment --
+   -----------------
+
+   function Environment
+     return League.Environment_Variables.Environment_Variable_Set is
+   begin
+      return X : League.Environment_Variables.Environment_Variable_Set;
+   end Environment;
+
+   -------------------------
+   -- Organization_Domain --
+   -------------------------
+
+   function Organization_Domain return League.Strings.Universal_String is
+   begin
+      return League.Strings.Empty_String;
+   end Organization_Domain;
+
+   -----------------------
+   -- Organization_Name --
+   -----------------------
+
+   function Organization_Name return League.Strings.Universal_String is
+   begin
+      return League.Strings.Empty_String;
+   end Organization_Name;
+
+   --------------------------
+   -- Set_Application_Name --
+   --------------------------
+
+   procedure Set_Application_Name (Name : League.Strings.Universal_String) is
+   begin
+      null;
+   end Set_Application_Name;
+
+   -----------------------------
+   -- Set_Application_Version --
+   -----------------------------
+
+   procedure Set_Application_Version
+    (Name : League.Strings.Universal_String) is
+   begin
+      null;
+   end Set_Application_Version;
+
+   -----------------------------
+   -- Set_Organization_Domain --
+   -----------------------------
+
+   procedure Set_Organization_Domain
+    (Name : League.Strings.Universal_String) is
+   begin
+      null;
+   end Set_Organization_Domain;
+
+   ---------------------------
+   -- Set_Organization_Name --
+   ---------------------------
+
+   procedure Set_Organization_Name (Name : League.Strings.Universal_String) is
+   begin
+      null;
+   end Set_Organization_Name;
+
+end League.Application;
