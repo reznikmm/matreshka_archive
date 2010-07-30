d1d4b07b93e900dba2ac09ab6e4407f753632f6c
Vadim Godunko <vgodunko@gmail.com>
2010-07-30 15:11:27 +0000

    Move declaration of the constant outside of the subprogram, to prevent GNAT to generate the same constant sections for each inline of the subprogram.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@853 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- source/league/matreshka-internals-strings-constants__x86_64.ads
+++ source/league/matreshka-internals-strings-constants__x86_64.ads
@@ -41,30 +41,30 @@
 ------------------------------------------------------------------------------
 --  $Revision$ $Date$
 ------------------------------------------------------------------------------
---  $Revision$ $Date$
+--  This package provides several constants for x86_64 platform.
 ------------------------------------------------------------------------------
 with Interfaces;
-
 with Matreshka.Internals.SIMD.Intel;
-with Matreshka.Internals.Strings.Constants;
+with Matreshka.Internals.Utf16;
 
-separate (Matreshka.Internals.Strings)
-procedure Fill_Null_Terminator (Self : not null Shared_String_Access) is
+private package Matreshka.Internals.Strings.Constants is
 
-   use Interfaces;
-   use Matreshka.Internals.SIMD.Intel;
-   use Matreshka.Internals.Strings.Constants;
+   pragma Preelaborate;
 
-   type v8hi_Unrestricted_Array is array (Utf16_String_Index) of v8hi;
+   use type Interfaces.Integer_16;
 
-   Value  : v8hi_Unrestricted_Array;
-   for Value'Address use Self.Value'Address;
-   Index  : constant Utf16_String_Index := Self.Unused / 8;
-   Offset : constant Utf16_String_Index := Self.Unused mod 8;
+   Terminator_Mask_x86_64 : constant
+     array (Matreshka.Internals.Utf16.Utf16_String_Index range 0 .. 7)
+       of Matreshka.Internals.SIMD.Intel.v8hi
+         := (0 => (              others => 0),
+             1 => (1      => -1, others => 0),
+             2 => (1 .. 2 => -1, others => 0),
+             3 => (1 .. 3 => -1, others => 0),
+             4 => (1 .. 4 => -1, others => 0),
+             5 => (1 .. 5 => -1, others => 0),
+             6 => (1 .. 6 => -1, others => 0),
+             7 => (1 .. 7 => -1, others => 0));
+   --  This mask is used to set unused components of the element to zero on
+   --  x86_64 platforms.
 
-begin
-   Value (Index) :=
-     To_v8hi
-      (mm_and_si128
-        (To_v2di (Value (Index)), To_v2di (Terminator_Mask_x86_64 (Offset))));
-end Fill_Null_Terminator;
+end Matreshka.Internals.Strings.Constants;
