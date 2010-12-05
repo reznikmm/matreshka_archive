9000b06b31b2f329512d4072153223cb4d7eb52a
Vadim Godunko <vgodunko@gmail.com>
2010-12-05 16:23:12 +0000

    Use Set_Content_Type to specify type of reply's content.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@1130 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- design/fastcgi/demo-callbacks.adb
+++ design/fastcgi/demo-callbacks.adb
@@ -1,10 +1,6 @@
 with League.Strings;
-with League.Text_Codecs;
 
 package body Demo.Callbacks is
-
-   Codec : League.Text_Codecs.Text_Codec
-     := League.Text_Codecs.Codec (League.Strings.To_Universal_String ("utf-8"));
 
    -------------
    -- Handler --
@@ -15,9 +11,8 @@ package body Demo.Callbacks is
      Reply   : out FastCGI.Replies.Reply;
      Status  : out Integer) is
    begin
-      Reply.Set_Raw_Header
-       (Codec.Encode (League.Strings.To_Universal_String ("Content-type")),
-        Codec.Encode (League.Strings.To_Universal_String ("text/plain")));
+      Reply.Set_Content_Type
+       (League.Strings.To_Universal_String ("text/plain"));
       Status := 0;
    end Handler;
 
