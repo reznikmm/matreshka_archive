6363865f8373dcd31c09322ff1564106589fc1d0
Vadim Godunko <vgodunko@gmail.com>
2011-01-13 22:46:02 +0000

    Update copyright year in generated files.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@1316 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- tools/gen_cases.adb
+++ tools/gen_cases.adb
@@ -8,7 +8,7 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2009-2010, Vadim Godunko <vgodunko@gmail.com>                --
+-- Copyright © 2009-2011, Vadim Godunko <vgodunko@gmail.com>                --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
@@ -319,7 +319,7 @@ begin
     (File,
      "Localization, Internationalization, Globalization for Ada",
      2009,
-     2010);
+     2011);
    Ada.Text_IO.New_Line (File);
    Ada.Text_IO.Put_Line
     (File, "package Matreshka.Internals.Unicode.Ucd.Cases is");
