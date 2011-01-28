06d74a490d065490d31d955422030c3db4af5313
Vadim Godunko <vgodunko@gmail.com>
2011-01-28 08:19:21 +0000

    Refactor convenience subprograms for writers to reuse them for other implementations of writers.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@1339 661adc5f-87f8-0310-85d6-9a7b5f58d823

---
+++ source/xml/xml-sax-writers.adb
@@ -0,0 +1,396 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               XML Processor                              --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2011, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+with Ada.Characters.Conversions;
+--  Use of this unit must be replaced to use of Text_Codec for application's
+--  locale.
+
+package body XML.SAX.Writers is
+
+   function To_Application_Locale_8_Bit_String
+    (Item : League.Strings.Universal_String) return String;
+   --  Converts Universal_String into standard String recoding it to encoding
+   --  of application's locale (not the current locale of the thread).
+
+   ----------------
+   -- Characters --
+   ----------------
+
+   procedure Characters
+    (Self : in out SAX_Writer'Class;
+     Text : League.Strings.Universal_String)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.Characters (Text, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end Characters;
+
+   -------------
+   -- Comment --
+   -------------
+
+   procedure Comment
+    (Self : in out SAX_Writer'Class;
+     Text : League.Strings.Universal_String)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.Comment (Text, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end Comment;
+
+   ---------------
+   -- End_CDATA --
+   ---------------
+
+   procedure End_CDATA (Self : in out SAX_Writer'Class) is
+      Success : Boolean := True;
+
+   begin
+      Self.End_CDATA (Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end End_CDATA;
+
+   ------------------
+   -- End_Document --
+   ------------------
+
+   procedure End_Document (Self : in out SAX_Writer'Class) is
+      Success : Boolean := True;
+
+   begin
+      Self.End_Document (Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end End_Document;
+
+   -------------
+   -- End_DTD --
+   -------------
+
+   procedure End_DTD (Self : in out SAX_Writer'Class) is
+      Success : Boolean := True;
+
+   begin
+      Self.End_DTD (Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end End_DTD;
+
+   -----------------
+   -- End_Element --
+   -----------------
+
+   procedure End_Element
+    (Self           : in out SAX_Writer'Class;
+     Namespace_URI  : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Local_Name     : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Qualified_Name : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.End_Element (Namespace_URI, Local_Name, Qualified_Name, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end End_Element;
+
+   ----------------
+   -- End_Entity --
+   ----------------
+
+   procedure End_Entity
+    (Self : in out SAX_Writer'Class;
+     Name : League.Strings.Universal_String)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.End_Entity (Name, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end End_Entity;
+
+   ------------------------
+   -- End_Prefix_Mapping --
+   ------------------------
+
+   procedure End_Prefix_Mapping
+    (Self   : in out SAX_Writer'Class;
+     Prefix : League.Strings.Universal_String)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.End_Entity (Prefix, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end End_Prefix_Mapping;
+
+   --------------------------
+   -- Ignorable_Whitespace --
+   --------------------------
+
+   procedure Ignorable_Whitespace
+    (Self : in out SAX_Writer'Class;
+     Text : League.Strings.Universal_String)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.Ignorable_Whitespace (Text, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end Ignorable_Whitespace;
+
+   ----------------------------
+   -- Processing_Instruction --
+   ----------------------------
+
+   procedure Processing_Instruction
+    (Self   : in out SAX_Writer'Class;
+     Target : League.Strings.Universal_String;
+     Data   : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.Processing_Instruction (Target, Data, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end Processing_Instruction;
+
+   --------------------
+   -- Skipped_Entity --
+   --------------------
+
+   procedure Skipped_Entity
+    (Self : in out SAX_Writer'Class;
+     Name : League.Strings.Universal_String)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.Skipped_Entity (Name, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end Skipped_Entity;
+
+   -----------------
+   -- Start_CDATA --
+   -----------------
+
+   procedure Start_CDATA (Self : in out SAX_Writer'Class) is
+      Success : Boolean := True;
+
+   begin
+      Self.Start_CDATA (Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end Start_CDATA;
+
+   --------------------
+   -- Start_Document --
+   --------------------
+
+   procedure Start_Document (Self : in out SAX_Writer'Class) is
+      Success : Boolean := True;
+
+   begin
+      Self.Start_Document (Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end Start_Document;
+
+   ---------------
+   -- Start_DTD --
+   ---------------
+
+   procedure Start_DTD
+    (Self      : in out SAX_Writer'Class;
+     Name      : League.Strings.Universal_String;
+     Public_Id : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     System_Id : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.Start_DTD (Name, Public_Id, System_Id, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end Start_DTD;
+
+   -------------------
+   -- Start_Element --
+   -------------------
+
+   procedure Start_Element
+    (Self           : in out SAX_Writer'Class;
+     Namespace_URI  : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Local_Name     : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Qualified_Name : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Attributes     : XML.SAX.Attributes.SAX_Attributes
+       := XML.SAX.Attributes.Empty_SAX_Attributes)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.Start_Element
+       (Namespace_URI, Local_Name, Qualified_Name, Attributes, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end Start_Element;
+
+   ------------------
+   -- Start_Entity --
+   ------------------
+
+   procedure Start_Entity
+    (Self : in out SAX_Writer'Class;
+     Name : League.Strings.Universal_String)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.Start_Entity (Name, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end Start_Entity;
+
+   --------------------------
+   -- Start_Prefix_Mapping --
+   --------------------------
+
+   procedure Start_Prefix_Mapping
+    (Self          : in out SAX_Writer'Class;
+     Prefix        : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Namespace_URI : League.Strings.Universal_String)
+   is
+      Success : Boolean := True;
+
+   begin
+      Self.Start_Prefix_Mapping (Prefix, Namespace_URI, Success);
+
+      if not Success then
+         raise Constraint_Error
+           with To_Application_Locale_8_Bit_String (Self.Error_String);
+      end if;
+   end Start_Prefix_Mapping;
+
+   ----------------------------------------
+   -- To_Application_Locale_8_Bit_String --
+   ----------------------------------------
+
+   function To_Application_Locale_8_Bit_String
+    (Item : League.Strings.Universal_String) return String is
+   begin
+      return Ada.Characters.Conversions.To_String (Item.To_Wide_Wide_String);
+      --  XXX This is stub, encoding of actual application's locale must be
+      --  used here.
+   end To_Application_Locale_8_Bit_String;
+
+end XML.SAX.Writers;
---
+++ source/xml/xml-sax-writers.ads
@@ -0,0 +1,170 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               XML Processor                              --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2011, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+--  This package declare base interface for SAX writers and set of convenience
+--  subprograms.
+------------------------------------------------------------------------------
+with League.Strings;
+with XML.SAX.Attributes;
+with XML.SAX.Content_Handlers;
+with XML.SAX.Lexical_Handlers;
+
+package XML.SAX.Writers is
+
+   type SAX_Writer is limited interface
+     and XML.SAX.Content_Handlers.SAX_Content_Handler
+       and XML.SAX.Lexical_Handlers.SAX_Lexical_Handler;
+
+   procedure Characters
+    (Self : in out SAX_Writer'Class;
+     Text : League.Strings.Universal_String);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when Characters sets Success to False.
+
+   procedure Comment
+    (Self : in out SAX_Writer'Class;
+     Text : League.Strings.Universal_String);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when Comment sets Success to False.
+
+   procedure End_CDATA (Self : in out SAX_Writer'Class);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when End_CDATA sets Success to False.
+
+   procedure End_Document (Self : in out SAX_Writer'Class);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when End_Document sets Success to False.
+
+   procedure End_DTD (Self : in out SAX_Writer'Class);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when End_DTD sets Success to False.
+
+   procedure End_Element
+    (Self           : in out SAX_Writer'Class;
+     Namespace_URI  : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Local_Name     : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Qualified_Name : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when End_Element sets Success to False.
+
+   procedure End_Entity
+    (Self : in out SAX_Writer'Class;
+     Name : League.Strings.Universal_String);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when End_Entity sets Success to False.
+
+   procedure End_Prefix_Mapping
+    (Self   : in out SAX_Writer'Class;
+     Prefix : League.Strings.Universal_String);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when End_Prefix_Mapping sets Success to False.
+
+   procedure Ignorable_Whitespace
+    (Self : in out SAX_Writer'Class;
+     Text : League.Strings.Universal_String);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when Ignorable_Whitespace sets Success to False.
+
+   procedure Processing_Instruction
+    (Self   : in out SAX_Writer'Class;
+     Target : League.Strings.Universal_String;
+     Data   : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when Processing_Instruction sets Success to False.
+
+   procedure Skipped_Entity
+    (Self : in out SAX_Writer'Class;
+     Name : League.Strings.Universal_String);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when Skipped_Entity sets Success to False.
+
+   procedure Start_CDATA (Self : in out SAX_Writer'Class);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when Start_CDATA sets Success to False.
+
+   procedure Start_Document (Self : in out SAX_Writer'Class);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when Start_Document sets Success to False.
+
+   procedure Start_DTD
+    (Self      : in out SAX_Writer'Class;
+     Name      : League.Strings.Universal_String;
+     Public_Id : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     System_Id : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when Start_DTD sets Success to False.
+
+   procedure Start_Element
+    (Self           : in out SAX_Writer'Class;
+     Namespace_URI  : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Local_Name     : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Qualified_Name : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Attributes     : XML.SAX.Attributes.SAX_Attributes
+       := XML.SAX.Attributes.Empty_SAX_Attributes);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when Start_Element sets Success to False.
+
+   procedure Start_Entity
+    (Self : in out SAX_Writer'Class;
+     Name : League.Strings.Universal_String);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when Start_Entity sets Success to False.
+
+   procedure Start_Prefix_Mapping
+    (Self          : in out SAX_Writer'Class;
+     Prefix        : League.Strings.Universal_String
+       := League.Strings.Empty_Universal_String;
+     Namespace_URI : League.Strings.Universal_String);
+   --  Overloaded subprogram for convenience. Raises Constraint_Error with
+   --  Error_String when Start_Prefix_Mapping sets Success to False.
+
+end XML.SAX.Writers;
