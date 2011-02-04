1f688c1a6ce1759e1c1fe8480561b6634db7468e
Vadim Godunko <vgodunko@gmail.com>
2011-02-04 07:18:38 +0000

    Remove use of standard String type and use Universal_String with Text_Codec to handle text data.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@1388 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- design/fastcgi/demo-callbacks.adb
+++ design/fastcgi/demo-callbacks.adb
@@ -7,6 +7,10 @@ with League.Text_Codecs;
 
 package body Demo.Callbacks is
 
+   function "+"
+    (Item : Wide_Wide_String) return League.Strings.Universal_String
+       renames League.Strings.To_Universal_String;
+
    function Less
     (Left  : League.Strings.Universal_String;
      Right : League.Strings.Universal_String) return Boolean;
@@ -17,39 +21,44 @@ package body Demo.Callbacks is
            Less,
            League.Strings."=");
 
-   Page_Top : constant String
-     := "<?xml version='1.1' encoding='utf-8'?>"
-          & "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>"
-          & "<html xmlns='http://www.w3.org/1999/xhtml'>"
-          & "<head>"
-          & "<title>Matreshka's FastCGI and Unicode Collation demo</title>"
-          & "</head>"
-          & "<body>"
-          & "<p>This simple FastCGI application sorts set of strings with Unicode Collation Algoriphm. You can enter own strings using text box and submit button.</p>"
-          & "<table>"
-          & "<tbody>";
+   Page_Top : constant League.Strings.Universal_String
+     := +("<?xml version='1.1' encoding='utf-8'?>"
+            & "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>"
+            & "<html xmlns='http://www.w3.org/1999/xhtml'>"
+            & "<head>"
+            & "<title>Matreshka's FastCGI and Unicode Collation demo</title>"
+            & "</head>"
+            & "<body>"
+            & "<p>This simple FastCGI application sorts set of strings with Unicode Collation Algoriphm. You can enter own strings using text box and submit button.</p>"
+            & "<table>"
+            & "<tbody>");
 
-   Page_Middle : constant String
-     := "</tbody>"
-          & "</table>"
-          & "<form action='";
+   Page_Middle : constant League.Strings.Universal_String
+     := +("</tbody>"
+            & "</table>"
+            & "<form action='");
 
-   Page_Bottom : constant String
-     := "' method='POST' accept-charset='utf-8'>"
-          & "<input type='text' name='string'/>"
-          & "<input type='submit' name='submit'/>"
-          & "</form>";
---          & "</body>"
---          & "</html>";
+   Page_Bottom : constant League.Strings.Universal_String
+     := +("' method='POST' accept-charset='utf-8'>"
+            & "<input type='text' name='string'/>"
+            & "<input type='submit' name='submit'/>"
+            & "</form>");
 
-   Page_Done : constant String
-     := "</body>"
-          & "</html>";
+   Page_Done : constant League.Strings.Universal_String
+     := +("</body>"
+            & "</html>");
 
-   Codec   : League.Text_Codecs.Text_Codec
+   Codec              : League.Text_Codecs.Text_Codec
      := League.Text_Codecs.Codec
          (League.Strings.To_Universal_String ("utf-8"));
-   Strings : Sets.Set;
+   String_Parameter   :
+     constant League.Stream_Element_Vectors.Stream_Element_Vector
+       := Codec.Encode (+"string");
+   Script_Name_Header :
+     constant League.Stream_Element_Vectors.Stream_Element_Vector
+       := Codec.Encode (+"SCRIPT_NAME");
+
+   Strings          : Sets.Set;
 
    -------------
    -- Handler --
@@ -64,9 +73,7 @@ package body Demo.Callbacks is
 
       procedure Output (Position : Sets.Cursor);
 
-      function To_Raw
-       (Item : String)
-          return League.Stream_Element_Vectors.Stream_Element_Vector;
+      procedure Put (Item : League.Strings.Universal_String);
 
       ------------
       -- Output --
@@ -77,33 +84,21 @@ package body Demo.Callbacks is
            := Sets.Element (Position);
 
       begin
-         String'Write (Reply.Stream, "<tr><td>");
-         Ada.Streams.Stream_Element_Array'Write
-          (Reply.Stream, Codec.Encode (Element).To_Stream_Element_Array);
-         String'Write (Reply.Stream, "<td></tr>");
+         Put (+"<tr><td>");
+         Put (Element);
+         Put (+"<td></tr>");
       end Output;
 
-      ------------
-      -- To_Raw --
-      ------------
+      ---------
+      -- Put --
+      ---------
 
-      function To_Raw
-       (Item : String)
-          return League.Stream_Element_Vectors.Stream_Element_Vector
-      is
-         Aux : Ada.Streams.Stream_Element_Array (1 .. Item'Length);
-         for Aux'Address use Item'Address;
-
+      procedure Put (Item : League.Strings.Universal_String) is
       begin
-         return League.Stream_Element_Vectors.To_Stream_Element_Vector (Aux);
-      end To_Raw;
-
-      String_Parameter :
-        constant League.Stream_Element_Vectors.Stream_Element_Vector
-          := To_Raw ("string");
-      Script_Name_Header :
-        constant League.Stream_Element_Vectors.Stream_Element_Vector
-          := To_Raw ("SCRIPT_NAME");
+         Ada.Streams.Stream_Element_Array'Write
+          (Reply.Stream,
+           Codec.Encode (Item).To_Stream_Element_Array);
+      end Put;
 
    begin
       if Request.Has_Raw_Query_Item (String_Parameter) then
@@ -115,15 +110,14 @@ package body Demo.Callbacks is
 
       Reply.Set_Content_Type
        (League.Strings.To_Universal_String ("text/html"));
---      Reply.Set_Status (200);
-      String'Write (Reply.Stream, Page_Top);
+      Put (Page_Top);
       Strings.Iterate (Output'Access);
-      String'Write (Reply.Stream, Page_Middle);
-      Ada.Streams.Stream_Element_Array'Write
-       (Reply.Stream,
-        Request.Raw_Header (Script_Name_Header).To_Stream_Element_Array);
-      String'Write (Reply.Stream, Page_Bottom);
-      String'Write (Reply.Stream, Page_Done);
+      Put (Page_Middle);
+      Put
+       (Codec.Decode
+         (Request.Raw_Header (Script_Name_Header).To_Stream_Element_Array));
+      Put (Page_Bottom);
+      Put (Page_Done);
       Status := 0;
    end Handler;
 
