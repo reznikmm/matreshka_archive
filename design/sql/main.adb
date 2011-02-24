2b3e1135517bb1c8f4b86ac372e2e644ec58b622
Vadim Godunko <vgodunko@gmail.com>
2011-02-24 22:45:20 +0000

    Extend example to show usage of integer and float values.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@1526 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- design/sql/main.adb
+++ design/sql/main.adb
@@ -1,7 +1,8 @@
 with Ada.Wide_Wide_Text_IO;
 
 with League.Strings;
-with League.Values;
+with League.Values.Floats;
+with League.Values.Integers;
 with Matreshka.Internals.SQL_Drivers.SQLite3.Factory;
 with SQL.Databases;
 with SQL.Queries;
@@ -26,7 +27,7 @@ begin
 
    begin
       Q.Prepare
-       (+"CREATE TABLE point (x CHARACTER VARYING, y CHARACTER VARYING)");
+       (+"CREATE TABLE point (x INTEGER, y CHARACTER VARYING, z FLOAT)");
       Q.Execute;
    end;
 
@@ -34,9 +35,10 @@ begin
       Q : SQL.Queries.SQL_Query := D.Query;
 
    begin
-      Q.Prepare (+"INSERT INTO point (x, y) VALUES (:x, :y)");
+      Q.Prepare (+"INSERT INTO point (x, y, z) VALUES (:x, :y, :z)");
+      Q.Bind_Value (+":z", League.Values.Floats.To_Value (4.5));
       Q.Bind_Value (+":y", League.Values.To_Value (+"xyz"));
-      Q.Bind_Value (+":x", League.Values.To_Value (+"abc"));
+      Q.Bind_Value (+":x", League.Values.Integers.To_Value (5));
       Q.Execute;
    end;
 
@@ -44,17 +46,22 @@ begin
       Q : aliased SQL.Queries.SQL_Query := D.Query;
 
    begin
-      Q.Prepare (+"SELECT x, y FROM point");
+      Q.Prepare (+"SELECT x, y, z FROM point");
       Q.Execute;
 
       while Q.Next loop
          Ada.Wide_Wide_Text_IO.Put_Line
-          (League.Values.Get (Q.Value (1)).To_Wide_Wide_String
+          (Integer'Wide_Wide_Image
+            (League.Values.Integers.Get (Q.Value (1)))
              & ":"
              & League.Values.Get (Q.Value (2)).To_Wide_Wide_String
-             & Integer'Wide_Wide_Image (League.Values.Get (Q.Value (1)).Length)
+             & ":"
+             & Float'Wide_Wide_Image
+                (League.Values.Floats.Get (Q.Value (3)))
+             & " (string length =>"
              & Integer'Wide_Wide_Image
-                (League.Values.Get (Q.Value (2)).Length));
+                (League.Values.Get (Q.Value (2)).Length)
+             & ")");
       end loop;
    end;
 
