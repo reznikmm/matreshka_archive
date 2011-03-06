9f40b8e3051450ac1378586fb16bee8e90231914
Vadim Godunko <vgodunko@gmail.com>
2011-03-06 19:51:58 +0000

    Remove dependency from SQLite3, reused subprogram is in base package structure now.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@1581 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- design/sql/oci/matreshka-internals-sql_drivers-oci-databases.adb
+++ design/sql/oci/matreshka-internals-sql_drivers-oci-databases.adb
@@ -45,7 +45,7 @@
 with Ada.Characters.Conversions;
 
 with League.Strings.Internals;
-with Matreshka.Internals.SQL_Drivers.SQLite3.String_Utilities;
+with Matreshka.Internals.Strings.C;
 with Matreshka.Internals.SQL_Drivers.OCI.Queries;
 
 with Ada.Wide_Wide_Text_IO;
@@ -106,8 +106,8 @@ package body Matreshka.Internals.SQL_Drivers.OCI.Databases is
                                   H_Type      => HT_Error);
 
                Self.Error_Text :=
-                 Matreshka.Internals.SQL_Drivers.SQLite3.String_Utilities.
-                   To_Universal_String (Buffer (0)'Unchecked_Access);
+                 Matreshka.Internals.Strings.C.To_Valid_Universal_String
+                  (Buffer (0)'Unchecked_Access);
 Ada.Wide_Wide_Text_IO.Put_Line (Self.Error_Text.To_Wide_Wide_String);
 
                return True;
