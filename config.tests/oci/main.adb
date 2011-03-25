96dbf90e10689670b2136efd23c2f6f84395b7e6
Vadim Godunko <vgodunko@gmail.com>
2011-03-25 20:49:48 +0000

    Update copyright information.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@1643 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- config.tests/oci/main.adb
+++ config.tests/oci/main.adb
@@ -8,7 +8,7 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2011, Maxim Reznik <reznikmm@gmail.com>                      --
+-- Copyright © 2011, Vadim Godunko <vgodunko@gmail.com>                     --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
