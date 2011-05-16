bc924ec904134b1fe14693307783f7b443116c4f
Vadim Godunko <vgodunko@gmail.com>
2011-05-16 17:51:13 +0000

    Add project file and package with enumerated type declarations for UML metamodel.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@1712 661adc5f-87f8-0310-85d6-9a7b5f58d823

---
+++ design/amf2/uml/amf-uml.ads
@@ -0,0 +1,85 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                          Ada Modeling Framework                          --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2011, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+
+package AMF.UML is
+
+   pragma Preelaborate;
+
+   type UML_Visibility_Kind is null record;
+
+   type Optional_UML_Visibility_Kind is null record;
+
+   type UML_Object_Node_Ordering_Kind is null record;
+
+   type UML_Transition_Kind is null record;
+
+   type UML_Expansion_Kind is null record;
+
+   type UML_Message_Kind is null record;
+
+   type UML_Message_Sort is null record;
+
+   type UML_Pseudostate_Kind is null record;
+
+   type UML_Aggregation_Kind is null record;
+
+   type UML_Interaction_Operator_Kind is null record;
+
+   type UML_Call_Concurrency_Kind is null record;
+
+   type UML_Parameter_Direction_Kind is null record;
+
+   type Optional_UML_Parameter_Effect_Kind is null record;
+
+   type UML_Connector_Kind is null record;
+
+   --  XXX Should be reused between meta models.
+
+   type Set_Of_Boolean is null record;
+
+   type Sequence_Of_String is null record;
+
+   type Ordered_Set_Of_String is null record;
+
+end AMF.UML;
