9435f8c4f38592b64eca6e7a7a9c382d8fbf534e
Dmitry Shtefan <dimashefxxx@gmail.com>
2011-07-29 13:23:37 +0000

    main file of the dhtml translator
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@2048 661adc5f-87f8-0310-85d6-9a7b5f58d823

---
+++ design/dhtml_translator/dhtml_translator.adb
@@ -0,0 +1,78 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--         Localization, Internationalization, Globalization for Ada        --
+--                                                                          --
+--                             Examples Component                           --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright B© 2011, Vadim Godunko <vgodunko@gmail.com>                    --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+
+with Ada.Command_Line;
+
+with League.Strings;
+with XML.SAX.Input_Sources.Streams.Files;
+with XML.SAX.Simple_Readers;
+
+with Events_Printers;
+with Put_Line;
+
+
+procedure DHTML_Translator is
+
+   function "+" (Item : Wide_Wide_String) return League.Strings.Universal_String
+     renames League.Strings.To_Universal_String;
+   Source  : aliased XML.SAX.Input_Sources.Streams.Files.File_Input_Source;
+   Reader  : aliased XML.SAX.Simple_Readers.SAX_Simple_Reader;
+   Handler : aliased Events_Printers.Events_Printer;
+begin
+   XML.SAX.Simple_Readers.Put_Line := Put_Line'Access;
+   Reader.Set_Content_Handler (Handler'Unchecked_Access);
+   Reader.Set_Declaration_Handler (Handler'Unchecked_Access);
+   Reader.Set_DTD_Handler (Handler'Unchecked_Access);
+   Reader.Set_Entity_Resolver (Handler'Unchecked_Access);
+   Reader.Set_Error_Handler (Handler'Unchecked_Access);
+   Reader.Set_Lexical_Handler (Handler'Unchecked_Access);
+
+   Source.Open (Ada.Command_Line.Argument (1));
+   Events_Printers.Create (Ada.Command_Line.Argument (2));
+   Events_Printers.Set_Resault_File_Name (Ada.Command_Line.Argument (2));
+
+   Reader.Parse (Source'Access);
+   Events_Printers.Close;
+
+end DHTML_Translator;
