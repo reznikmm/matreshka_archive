3822738b4e42b8ce44fe296cc752703ebd171307
Maxim Reznik <reznikmm@gmail.com>
2011-08-03 08:05:53 +0000

    Add time conversion package
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@2068 661adc5f-87f8-0310-85d6-9a7b5f58d823

---
+++ source/league/league-calendars-ada_conversions.adb
@@ -0,0 +1,76 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--         Localization, Internationalization, Globalization for Ada        --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2011, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+
+with Ada.Calendar.Time_Zones;
+with League.Calendars.ISO_8601;
+
+package body League.Calendars.Ada_Conversions is
+
+   -------------------
+   -- From_Ada_Time --
+   -------------------
+
+   function From_Ada_Time (Time : Ada.Calendar.Time) return Date_Time is
+      use League.Calendars.ISO_8601;
+      use type Ada.Calendar.Time;
+
+      Year    : Ada.Calendar.Year_Number;
+      Month   : Ada.Calendar.Month_Number;
+      Day     : Ada.Calendar.Day_Number;
+      Seconds : Ada.Calendar.Day_Duration;
+
+      Offset  : constant Ada.Calendar.Time_Zones.Time_Offset :=
+        Ada.Calendar.Time_Zones.UTC_Time_Offset (Time);
+
+      UTC     : constant Ada.Calendar.Time := Time - Duration (Offset) * 60.0;
+   begin
+      Ada.Calendar.Split (UTC, Year, Month, Day, Seconds);
+
+      return Create (Year_Number (Year),
+                     Month_Number (Month),
+                     Day_Number (Day),
+                     League.Calendars.Time (Seconds * 1e7));
+   end From_Ada_Time;
+
+end League.Calendars.Ada_Conversions;
