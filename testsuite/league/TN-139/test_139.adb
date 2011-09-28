0904cc975e636369f2ae04a4ab5252b63123c2e5
Vadim Godunko <vgodunko@gmail.com>
2011-09-28 10:39:15 +0000

    Add automated test.
    Refs #139.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@2126 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- testsuite/league/TN-139/test_139.adb
+++ testsuite/league/TN-139/test_139.adb
@@ -4,11 +4,11 @@
 --                                                                          --
 --         Localization, Internationalization, Globalization for Ada        --
 --                                                                          --
---                              Tools Component                             --
+--                            Testsuite Component                           --
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2009-2011, Vadim Godunko <vgodunko@gmail.com>                --
+-- Copyright © 2011, Vadim Godunko <vgodunko@gmail.com>                     --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
@@ -41,42 +41,14 @@
 ------------------------------------------------------------------------------
 --  $Revision$ $Date$
 ------------------------------------------------------------------------------
-with "matreshka_common.gpr";
-with "matreshka_league.gpr";
+--  This test checks whether Empty_Holder is initialized properly and copy of
+--  Empty_Holder works right.
+------------------------------------------------------------------------------
+with League.Holders;
 
-project Matreshka_League_Tests is
+procedure Test_139 is
+   H : League.Holders.Holder;
 
-   for Main use
-    ("string_hash_test.adb",
-     "character_cursor_test.adb",
-     "grapheme_cluster_cursor_test.adb",
-     "case_conversion_test.adb",
-     "case_folding_test.adb",
-     "normalization_test.adb",
-     "additional_normalization_test.adb",
-     "collation_test.adb",
-     "string_performance.adb",
-     "fill_null_terminator_performance.adb",
-     "string_operations_test.adb",
-     "string_compare_test.adb",
-     "library_level_test.adb",
-     "regexp_ataresearch.adb",
-     "test_35.adb",
-     "test_104.adb",
-     "test_106.adb",
-     "test_139.adb",
-     "arguments_environment_test.adb");
-   for Object_Dir use "../.objs";
-   for Source_Dirs use
-    ("../testsuite/league",
-     "../testsuite/league/TN-35",
-     "../testsuite/league/TN-104",
-     "../testsuite/league/TN-106",
-     "../testsuite/league/TN-139",
-     "../tools");
-
-   package Compiler is
-      for Default_Switches ("Ada") use Matreshka_Common.Common_Ada_Switches;
-   end Compiler;
-
-end Matreshka_League_Tests;
+begin
+   H := League.Holders.Empty_Holder;
+end Test_139;
