6b421bcb7d2a68c594cab26846ac3389aa61d21d
Vadim Godunko <vgodunko@gmail.com>
2012-02-02 14:57:01 +0000

    Merge fix into 0.2.x branch.
    Refs #180.
    
    
    git-svn-id: file:///srv/svn/matreshka/branches/matreshka-0.2.x@2458 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- tools/configure/configure-component_switches.adb
+++ tools/configure/configure-component_switches.adb
@@ -8,7 +8,7 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2011, Vadim Godunko <vgodunko@gmail.com>                     --
+-- Copyright © 2011-2012, Vadim Godunko <vgodunko@gmail.com>                --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
@@ -207,7 +207,7 @@ package body Configure.Component_Switches is
             then
                Self.Libdir :=
                  Unbounded_Slice
-                  (Argument, Length (Libdir) + 1, Length (Argument));
+                  (Argument, Length (Libdir) + 2, Length (Argument));
                Self.Libdir_Specified := True;
                Self.Enable := True;
                Arguments.Delete (Index);
