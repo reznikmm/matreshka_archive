b225d7a3c877598b89b1899cd0e4a8cb3e8a392b
Vadim Godunko <vgodunko@gmail.com>
2012-03-01 16:43:32 +0000

    Add notification subprograms for PrimitiveTypes package.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@2544 661adc5f-87f8-0310-85d6-9a7b5f58d823

---
+++ source/amf/uml/amf-internals-tables-primitive_types_notification.ads
@@ -0,0 +1,104 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                          Ada Modeling Framework                          --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2012, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+--  Helper subprograms for element modification notification.
+------------------------------------------------------------------------------
+with Matreshka.Internals.Strings;
+
+package AMF.Internals.Tables.Primitive_Types_Notification is
+
+   procedure Notify_Attribute_Set
+    (Element   : AMF.Internals.AMF_Element;
+     Property  : AMF.Internals.CMOF_Element;
+     Old_Value : Boolean;
+     New_Value : Boolean);
+
+--   procedure Notify_Attribute_Set
+--    (Element   : AMF.Internals.UML_Element;
+--     Property  : AMF.Internals.CMOF_Element;
+--     Old_Value : AMF.Optional_Boolean;
+--     New_Value : AMF.Optional_Boolean);
+
+   procedure Notify_Attribute_Set
+    (Element   : AMF.Internals.AMF_Element;
+     Property  : AMF.Internals.CMOF_Element;
+     Old_Value : Matreshka.Internals.Strings.Shared_String_Access;
+     New_Value : Matreshka.Internals.Strings.Shared_String_Access);
+
+   procedure Notify_Attribute_Set
+    (Element   : AMF.Internals.AMF_Element;
+     Property  : AMF.Internals.CMOF_Element;
+     Old_Value : Integer;
+     New_Value : Integer);
+
+--   procedure Notify_Attribute_Set
+--    (Element   : AMF.Internals.UML_Element;
+--     Property  : AMF.Internals.CMOF_Element;
+--     Old_Value : AMF.Optional_Integer;
+--     New_Value : AMF.Optional_Integer);
+
+   procedure Notify_Attribute_Set
+    (Element   : AMF.Internals.AMF_Element;
+     Property  : AMF.Internals.CMOF_Element;
+     Old_Value : AMF.Unlimited_Natural;
+     New_Value : AMF.Unlimited_Natural);
+
+--   procedure Notify_Attribute_Set
+--    (Element   : AMF.Internals.UML_Element;
+--     Property  : AMF.Internals.CMOF_Element;
+--     Old_Value : AMF.Optional_Unlimited_Natural;
+--     New_Value : AMF.Optional_Unlimited_Natural);
+
+   procedure Notify_Attribute_Set
+    (Element   : AMF.Internals.AMF_Element;
+     Property  : AMF.Internals.CMOF_Element;
+     Old_Value : AMF.Real;
+     New_Value : AMF.Real);
+
+--   procedure Notify_Attribute_Set
+--    (Element   : AMF.Internals.UML_Element;
+--     Property  : AMF.Internals.CMOF_Element;
+--     Old_Value : AMF.Internals.UML_Element;
+--     New_Value : AMF.Internals.UML_Element);
+
+end AMF.Internals.Tables.Primitive_Types_Notification;
