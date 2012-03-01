8220f45ac7911ba6bc9c8cc34428bd44847ea707
Vadim Godunko <vgodunko@gmail.com>
2012-03-01 17:13:52 +0000

    Regenerate code.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@2556 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- source/amf/uml/amf-internals-tables-uml_constructors.adb
+++ source/amf/uml/amf-internals-tables-uml_constructors.adb
@@ -8,7 +8,7 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2010-2011, Vadim Godunko <vgodunko@gmail.com>                --
+-- Copyright © 2011-2012, Vadim Godunko <vgodunko@gmail.com>                --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
