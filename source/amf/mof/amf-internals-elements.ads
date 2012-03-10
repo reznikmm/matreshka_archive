3bcbe9c7dd333dbacfd6700c92629f7d1d951f08
Vadim Godunko <vgodunko@gmail.com>
2012-03-10 17:54:23 +0000

    Add operation to dispatch calls to metamodel specific iterator.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@2570 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- source/amf/mof/amf-internals-elements.ads
+++ source/amf/mof/amf-internals-elements.ads
@@ -8,7 +8,7 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2011, Vadim Godunko <vgodunko@gmail.com>                     --
+-- Copyright © 2011-2012, Vadim Godunko <vgodunko@gmail.com>                --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
@@ -76,5 +76,11 @@ package AMF.Internals.Elements is
      Visitor : not null access AMF.Visitors.Abstract_Visitor'Class;
      Control : in out AMF.Visitors.Traverse_Control) is abstract;
    --  Dispatch call to corresponding subprogram of visitor interface.
+
+   not overriding procedure Visit_Element
+    (Self     : not null access constant Element_Implementation;
+     Iterator : not null access AMF.Visitors.Abstract_Iterator'Class;
+     Control  : in out AMF.Visitors.Traverse_Control) is abstract;
+   --  Dispatch call to corresponding subprogram of iterator interface.
 
 end AMF.Internals.Elements;
