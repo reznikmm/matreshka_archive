ea6882ba67cf61140e82fe534ead86d325a35c91
Vadim Godunko <vgodunko@gmail.com>
2012-04-10 17:46:31 +0000

    Remove commented out code.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@2826 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- source/amf/mof/amf-internals-extents.adb
+++ source/amf/mof/amf-internals-extents.adb
@@ -49,20 +49,10 @@ with AMF.Internals.AMF_URI_Extents;
 with AMF.Internals.AMF_URI_Stores;
 with AMF.Internals.Collections.Elements.Containers;
 with AMF.Internals.Helpers;
---with AMF.Internals.Element_Collections;
 with AMF.Internals.Tables.AMF_Tables;
---with CMOF.Internals.Attribute_Mappings;
---with CMOF.Internals.Attributes;
---with CMOF.Internals.Reflection;
---with CMOF.Internals.Metamodel;
---with CMOF.Internals.Subclassing;
---with CMOF.Internals.Tables;
 
 package body AMF.Internals.Extents is
 
---   use AMF.Internals;
---   use AMF.Internals.Tables;
---   use type AMF.Internals.AMF_Element;
    use type AMF.Internals.Tables.AMF_Tables.Extent_Element_Identifier;
    use type Matreshka.Internals.Strings.Shared_String_Access;
 
@@ -145,135 +135,6 @@ package body AMF.Internals.Extents is
            (AMF.Internals.Tables.AMF_Tables.Extents.Last).Proxy);
    end Allocate_URI_Store;
 
---   ---------------
---   -- Container --
---   ---------------
---
---   function Container (Self : CMOF_Element) return CMOF_Element is
---
---      use AMF.Internals.Element_Collections;
---      use CMOF.Internals.Attribute_Mappings;
---      use CMOF.Internals.Attributes;
---      use CMOF.Internals.Reflection;
---      use CMOF.Internals.Metamodel;
---      use CMOF.Internals.Subclassing;
---      use type AMF.Internals.Tables.AMF_Tables.Collection_Element_Identifier;
---
---      Meta_Class      : constant CMOF_Class
---        := Reflection.Get_Meta_Class (Self);
---      Meta_Extent     : constant AMF_Extent
---        := Tables.Elements.Table (Meta_Class).Extent;
---      Current         : AMF_Tables.Extent_Element_Identifier
---        := AMF.Internals.Tables.AMF_Tables.Extents.Table (Meta_Extent).Head;
---      Current_Element : AMF_Element;
---      Association     : CMOF_Association;
---      Member_End      : Ordered_Set_Of_CMOF_Property;
---      Opposite        : CMOF_Property;
---      Collection      : AMF_Tables.Collection_Element_Identifier;
---      Owner           : CMOF_Element;
---      Link            : AMF.Internals.AMF_Link;
---
---   begin
---      --  All properties must be traversed to detect containment.
---
---      while Current /= 0 loop
---         Current_Element := AMF_Tables.Extent_Elements.Table (Current).Element;
---
---         if Is_Property (Current_Element)
---           and then Internal_Get_Type (Current_Element) in CMOF_Meta_Class
---           and then Is_Subclass
---                     (Meta_Class, Internal_Get_Type (Current_Element))
---           and then Boolean (Internal_Get_Is_Composite (Current_Element))
---         then
---            --  Find opposite property, it is at the element's side of the
---            --  association.
---
---            Association := Internal_Get_Association (Current_Element);
---            Member_End := Internal_Get_Member_End (Association);
---
---            if Element (Member_End, 1) = Current_Element then
---               Opposite := Element (Member_End, 2);
---
---            else
---               Opposite := Element (Member_End, 1);
---            end if;
---
---            --  Depending of multiplicity and ownedship of property, locate
---            --  member or collection and check presence of link. Note: this
---            --  must be reflective operation, but now it is hardcoded at
---            --  CMOF level.
---
---            if Opposite in Cmof_Non_Collection_Of_Element_Property'Range then
---               if Member_Offset
---                   (Tables.Elements.Table (Self).Kind, Opposite) /= 0
---               then
---                  Owner :=
---                    Tables.Elements.Table (Self).Member
---                     (Member_Offset
---                       (Tables.Elements.Table (Self).Kind,
---                       Opposite)).Element;
---
---                  if Owner /= Null_CMOF_Element then
---                     return Owner;
---                  end if;
---
---               else
---               Collection :=
---                 AMF_Tables.Collections.Table
---                  (Tables.Elements.Table (Self).Member (0).Collection).Head;
---
---               while Collection /= 0 loop
---                  Link :=
---                    AMF_Tables.Collection_Elements.Table (Collection).Link;
---
---                  if AMF_Tables.Links.Table (Link).Association
---                       = Association
---                  then
---                     if AMF_Tables.Links.Table (Link).First_Element = Self then
---                        return AMF_Tables.Links.Table (Link).Second_Element;
---
---                     else
---                        return AMF_Tables.Links.Table (Link).First_Element;
---                     end if;
---                  end if;
---
---                  Collection :=
---                    AMF_Tables.Collection_Elements.Table (Collection).Next;
---               end loop;
---               end if;
---
---            else
---               Collection :=
---                 AMF_Tables.Collections.Table
---                  (Tables.Elements.Table (Self).Member (0).Collection).Head;
---
---               while Collection /= 0 loop
---                  Link :=
---                    AMF_Tables.Collection_Elements.Table (Collection).Link;
---
---                  if AMF_Tables.Links.Table (Link).Association
---                       = Association
---                  then
---                     if AMF_Tables.Links.Table (Link).First_Element = Self then
---                        return AMF_Tables.Links.Table (Link).Second_Element;
---
---                     else
---                        return AMF_Tables.Links.Table (Link).First_Element;
---                     end if;
---                  end if;
---
---                  Collection :=
---                    AMF_Tables.Collection_Elements.Table (Collection).Next;
---               end loop;
---            end if;
---         end if;
---
---         Current := AMF_Tables.Extent_Elements.Table (Current).Next;
---      end loop;
---
---      return Null_CMOF_Element;
---   end Container;
-
    -------------
    -- Element --
    -------------
@@ -300,58 +161,6 @@ package body AMF.Internals.Extents is
 
       return 0;
    end Element;
-
---   -------------
---   -- Element --
---   -------------
---
---   function Element
---    (Self  : CMOF_Extent;
---     Index : Positive) return CMOF_Element
---   is
---      Current : AMF_Tables.Extent_Element_Identifier
---        := AMF.Internals.Tables.AMF_Tables.Extents.Table (Self).Head;
---
---   begin
---      for J in 2 .. Index loop
---         exit when Current = 0;
---
---         Current := AMF_Tables.Extent_Elements.Table (Current).Next;
---      end loop;
---
---      if Current = 0 then
---         raise Constraint_Error;
---
---      else
---         return AMF_Tables.Extent_Elements.Table (Current).Element;
---      end if;
---   end Element;
---
---   --------------
---   -- Elements --
---   --------------
---
---   function All_Elements
---    (Self : CMOF_Extent)
---       return not null AMF.Internals.Collections.Collection_Access
---   is
---      Current  : AMF_Tables.Extent_Element_Identifier
---        := AMF_Tables.Extents.Table (Self).Head;
---      Result   : not null AMF.Internals.Collections.Collection_Access
---        := new AMF.Internals.Containers.Collection;
---      Elements : AMF.Internals.Containers.Vectors.Vector
---        renames AMF.Internals.Containers.Collection (Result.all).Elements;
---
---   begin
---      while Current /= 0 loop
---         Elements.Append
---          (AMF.Internals.Helpers.To_Element
---            (AMF_Tables.Extent_Elements.Table (Current).Element));
---         Current := AMF_Tables.Extent_Elements.Table (Current).Next;
---      end loop;
---
---      return Result;
---   end All_Elements;
 
    ---------------------
    -- Internal_Append --
@@ -427,24 +236,6 @@ package body AMF.Internals.Extents is
         AMF.Extents.Extent_Access
          (AMF.Internals.Tables.AMF_Tables.Extents.Table (Extent).Proxy);
    end Proxy;
-
---   ------------
---   -- Length --
---   ------------
---
---   function Length (Self : CMOF_Extent) return Natural is
---      Current : AMF_Tables.Extent_Element_Identifier
---        := AMF.Internals.Tables.AMF_Tables.Extents.Table (Self).Head;
---      Aux     : Natural := 0;
---
---   begin
---      while Current /= 0 loop
---         Aux     := Aux + 1;
---         Current := AMF_Tables.Extent_Elements.Table (Current).Next;
---      end loop;
---
---      return Aux;
---   end Length;
 
    ------------
    -- Set_Id --
