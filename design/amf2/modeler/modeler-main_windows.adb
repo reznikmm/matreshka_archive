c9e4c88eac8effed816697f13c22e648ae0a4db4
Vadim Godunko <vgodunko@gmail.com>
2012-04-23 03:38:35 +0000

    Create MDI area and menu bar for main window.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@2893 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- design/amf2/modeler/modeler-main_windows.adb
+++ design/amf2/modeler/modeler-main_windows.adb
@@ -41,6 +41,10 @@
 ------------------------------------------------------------------------------
 --  $Revision$ $Date$
 ------------------------------------------------------------------------------
+with Qt4.Mdi_Areas.Constructors;
+with Qt4.Menu_Bars.Constructors;
+--with Qt4.Status_Bars.Constructors;
+
 with Modeler.Main_Windows.Moc;
 pragma Unreferenced (Modeler.Main_Windows.Moc);
 
@@ -52,6 +56,9 @@ package body Modeler.Main_Windows is
 
    package body Constructors is
 
+      procedure Initialize (Self : not null access Main_Window'Class);
+      --  Initialize widget.
+
       ------------
       -- Create --
       ------------
@@ -61,9 +68,38 @@ package body Modeler.Main_Windows is
          return Self : constant not null Main_Window_Access
            := new Main_Window
          do
-            Qt4.Main_Windows.Directors.Constructors.Initialize (Self);
+            Initialize (Self);
          end return;
       end Create;
+
+      ----------------
+      -- Initialize --
+      ----------------
+
+      procedure Initialize (Self : not null access Main_Window'Class) is
+         Mdi_Area   : Qt4.Mdi_Areas.Q_Mdi_Area_Access;
+         Menu_Bar   : Qt4.Menu_Bars.Q_Menu_Bar_Access;
+--         Status_Bar : Qt4.Status_Bars.Q_Status_Bar_Access;
+
+      begin
+         Qt4.Main_Windows.Directors.Constructors.Initialize (Self);
+
+         --  Create menu bar.
+
+         Menu_Bar := Qt4.Menu_Bars.Constructors.Create (Self);
+         Self.Set_Menu_Bar (Menu_Bar);
+
+         --  Create MDI area.
+
+         Mdi_Area := Qt4.Mdi_Areas.Constructors.Create (Self);
+         Self.Set_Central_Widget (Mdi_Area);
+
+         --  Create status bar.
+
+--  XXX Qt4.Status_Bars.Constructors is not implemented.
+--         Status_Bar := Qt4.Status_Bars.Constructors.Create (Self);
+--         Self.Set_Status_Bar (Status_Bar);
+      end Initialize;
 
    end Constructors;
 
