83d241e8a946ed26e71d8055b0480bf39cdecaaf
Vadim Godunko <vgodunko@gmail.com>
2012-08-13 07:38:37 +0000

    Use Ada 2012 syntax sugar for SAX_Attributes type.
    Refs #238.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@3050 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- source/xml/sax/xml-sax-attributes.ads
+++ source/xml/sax/xml-sax-attributes.ads
@@ -8,7 +8,7 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2010-2011, Vadim Godunko <vgodunko@gmail.com>                --
+-- Copyright © 2010-2012, Vadim Godunko <vgodunko@gmail.com>                --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
@@ -53,7 +53,10 @@ package XML.SAX.Attributes is
 
    pragma Preelaborate;
 
-   type SAX_Attributes is tagged private;
+   type SAX_Attributes is tagged private
+     with Iterator_Element  => League.Strings.Universal_String,
+          Constant_Indexing => Value;
+   pragma Preelaborable_Initialization (SAX_Attributes);
 
    Empty_SAX_Attributes : constant SAX_Attributes;
 
