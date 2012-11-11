78b5b7c4b3c763f9ea7d1127d569bba5b94c9bbc
Vadim Godunko <vgodunko@gmail.com>
2012-11-11 05:42:20 +0000

    Add factories for DD module.
    Refs #256.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@3373 661adc5f-87f8-0310-85d6-9a7b5f58d823

---
+++ source/amf/dd/amf-internals-modules-dd_module.adb
@@ -0,0 +1,105 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                          Ada Modeling Framework                          --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2012, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+with AMF.Internals.Factories.DC_Factories;
+with AMF.Internals.Factories.DG_Factories;
+with AMF.Internals.Factories.DI_Factories;
+with AMF.Internals.Factories.DD_Module_Factory;
+with AMF.Internals.Tables.DC_Metamodel.Links;
+with AMF.Internals.Tables.DC_Metamodel.Objects;
+with AMF.Internals.Tables.DC_Metamodel.Properties;
+with AMF.Internals.Tables.DD_Element_Table;
+with AMF.Internals.Tables.DG_Metamodel.Links;
+with AMF.Internals.Tables.DG_Metamodel.Objects;
+with AMF.Internals.Tables.DG_Metamodel.Properties;
+with AMF.Internals.Tables.DI_Metamodel.Links;
+with AMF.Internals.Tables.DI_Metamodel.Objects;
+with AMF.Internals.Tables.DI_Metamodel.Properties;
+
+with AMF.Internals.Modules.CMOF_Module;
+pragma Unreferenced (AMF.Internals.Modules.CMOF_Module);
+pragma Elaborate_All (AMF.Internals.Modules.CMOF_Module);
+--  CMOF nodule package and all its dependencies must be elaborated before
+--  elaboration of this package.
+
+package body AMF.Internals.Modules.DD_Module is
+
+   --  Global object of factory for supported metamodel.
+
+   DD_Module_Factory : aliased
+     AMF.Internals.Factories.DD_Module_Factory.DD_Module_Factory;
+   Module            : AMF_Metamodel;
+
+begin
+   --  Register module's factory.
+
+   AMF.Internals.Factories.Register (DD_Module_Factory'Access, Module);
+
+   --  Initialize metamodels.
+
+   AMF.Internals.Tables.DC_Metamodel.Objects.Initialize;
+   AMF.Internals.Tables.DG_Metamodel.Objects.Initialize;
+   AMF.Internals.Tables.DI_Metamodel.Objects.Initialize;
+   AMF.Internals.Tables.DC_Metamodel.Properties.Initialize;
+   AMF.Internals.Tables.DG_Metamodel.Properties.Initialize;
+   AMF.Internals.Tables.DI_Metamodel.Properties.Initialize;
+   AMF.Internals.Tables.DC_Metamodel.Links.Initialize;
+   AMF.Internals.Tables.DG_Metamodel.Links.Initialize;
+   AMF.Internals.Tables.DI_Metamodel.Links.Initialize;
+
+   --  Initialize element table of DD metamodel.
+
+   AMF.Internals.Tables.DD_Element_Table.Initialize (Module);
+
+   --  Register factories.
+
+   AMF.Internals.Factories.Register
+    (AMF.Internals.Factories.DC_Factories.Get_Package,
+     AMF.Internals.Factories.DC_Factories.Constructor'Access);
+   AMF.Internals.Factories.Register
+    (AMF.Internals.Factories.DG_Factories.Get_Package,
+     AMF.Internals.Factories.DG_Factories.Constructor'Access);
+   AMF.Internals.Factories.Register
+    (AMF.Internals.Factories.DI_Factories.Get_Package,
+     AMF.Internals.Factories.DI_Factories.Constructor'Access);
+end AMF.Internals.Modules.DD_Module;
