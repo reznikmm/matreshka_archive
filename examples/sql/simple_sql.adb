13b043ec9125901b830b405b1ca409ed16edcb5b
Vadim Godunko <vgodunko@gmail.com>
2012-12-25 11:18:54 +0000

    Update example to use latest API to provide database specific options.
    Refs #272.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@3613 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- examples/sql/simple_sql.adb
+++ examples/sql/simple_sql.adb
@@ -8,7 +8,7 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2011, Vadim Godunko <vgodunko@gmail.com>                     --
+-- Copyright © 2011-2012, Vadim Godunko <vgodunko@gmail.com>                --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
@@ -46,11 +46,13 @@ with Ada.Wide_Wide_Text_IO;
 with League.Strings;
 with League.Holders.Floats;
 with League.Holders.Integers;
+--with Matreshka.Internals.SQL_Drivers.Firebird.Factory;
+--with Matreshka.Internals.SQL_Drivers.MySQL.Factory;
 --with Matreshka.Internals.SQL_Drivers.Oracle.Factory;
 --with Matreshka.Internals.SQL_Drivers.PostgreSQL.Factory;
---with Matreshka.Internals.SQL_Drivers.Firebird.Factory;
 with Matreshka.Internals.SQL_Drivers.SQLite3.Factory;
 with SQL.Databases;
+with SQL.Options;
 with SQL.Queries;
 
 procedure Simple_SQL is
@@ -59,72 +61,83 @@ procedure Simple_SQL is
     (Item : Wide_Wide_String) return League.Strings.Universal_String
        renames League.Strings.To_Universal_String;
 
-   --  Oracle
+--   DB_Driver : constant League.Strings.Universal_String := +"MYSQL";
+   --  MySQL
 
 --   DB_Type : constant League.Strings.Universal_String := +"ORACLE";
 --   DB_Name : constant League.Strings.Universal_String := +"scott/tiger@db";
+   --  Oracle
 
+--   DB_Driver : constant League.Strings.Universal_String := +"POSTGRESQL";
    --  PostgreSQL
 
---   DB_Driver  : constant League.Strings.Universal_String := +"POSTGRESQL";
---   DB_Options : constant League.Strings.Universal_String := +"";
-   --  Example: +"user='me' password='my' dbname='db'"
-
+   DB_Driver : constant League.Strings.Universal_String := +"SQLITE3";
    --  SQLite3
-
-   DB_Driver  : constant League.Strings.Universal_String := +"SQLITE3";
-   DB_Options : constant League.Strings.Universal_String := +"test.db";
-
-   --  Firebird
 
 --   DB_Driver  : constant League.Strings.Universal_String := +"FIREBIRD";
 --   DB_Options : constant League.Strings.Universal_String :=
 --     +"SYSDBA/masterkey@localhost:/tmp/aaa";
+   --  Firebird
 
-   D : aliased SQL.Databases.SQL_Database
-     := SQL.Databases.Create (DB_Driver, DB_Options);
+   DB_Options : SQL.Options.SQL_Options;
 
 begin
-   D.Open;
+   --  MySQL
+
+--   DB_Options.Set (+"database", +"test");
+
+   --  PostgreSQL
+
+   --  SQLite3
+
+   DB_Options.Set (+"filename", +"test.db");
 
    declare
-      Q : SQL.Queries.SQL_Query := D.Query;
+      D : aliased SQL.Databases.SQL_Database
+        := SQL.Databases.Create (DB_Driver, DB_Options);
 
    begin
-      Q.Prepare
-       (+"CREATE TABLE point (x INTEGER, y CHARACTER VARYING (6), z FLOAT)");
-      Q.Execute;
+      D.Open;
+
+      declare
+         Q : SQL.Queries.SQL_Query := D.Query;
+
+      begin
+         Q.Prepare
+          (+"CREATE TABLE point (x INTEGER, y CHARACTER VARYING (6), z FLOAT)");
+         Q.Execute;
+      end;
+
+      declare
+         Q : SQL.Queries.SQL_Query := D.Query;
+
+      begin
+         Q.Prepare (+"INSERT INTO point (x, y, z) VALUES (:x, :y, :z)");
+         Q.Bind_Value (+":z", League.Holders.Floats.To_Holder (4.5));
+         Q.Bind_Value (+":y", League.Holders.To_Holder (+"xyz"));
+         Q.Bind_Value (+":x", League.Holders.Integers.To_Holder (5));
+         Q.Execute;
+      end;
+
+      declare
+         Q : aliased SQL.Queries.SQL_Query := D.Query;
+
+      begin
+         Q.Prepare (+"SELECT x, y, z FROM point");
+         Q.Execute;
+
+         while Q.Next loop
+            Ada.Wide_Wide_Text_IO.Put_Line
+             (Integer'Wide_Wide_Image
+               (League.Holders.Integers.Element (Q.Value (1)))
+                & ":"
+                & League.Holders.Element (Q.Value (2)).To_Wide_Wide_String
+                & ":"
+                & Float'Wide_Wide_Image
+                   (League.Holders.Floats.Element (Q.Value (3))));
+         end loop;
+      end;
+
+      D.Close;
    end;
-
-   declare
-      Q : SQL.Queries.SQL_Query := D.Query;
-
-   begin
-      Q.Prepare (+"INSERT INTO point (x, y, z) VALUES (:x, :y, :z)");
-      Q.Bind_Value (+":z", League.Holders.Floats.To_Holder (4.5));
-      Q.Bind_Value (+":y", League.Holders.To_Holder (+"xyz"));
-      Q.Bind_Value (+":x", League.Holders.Integers.To_Holder (5));
-      Q.Execute;
-   end;
-
-   declare
-      Q : aliased SQL.Queries.SQL_Query := D.Query;
-
-   begin
-      Q.Prepare (+"SELECT x, y, z FROM point");
-      Q.Execute;
-
-      while Q.Next loop
-         Ada.Wide_Wide_Text_IO.Put_Line
-          (Integer'Wide_Wide_Image
-            (League.Holders.Integers.Element (Q.Value (1)))
-             & ":"
-             & League.Holders.Element (Q.Value (2)).To_Wide_Wide_String
-             & ":"
-             & Float'Wide_Wide_Image
-                (League.Holders.Floats.Element (Q.Value (3))));
-      end loop;
-   end;
-
-   D.Close;
 end Simple_SQL;
