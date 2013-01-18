ba1a55a637ba2031124bdf4640d35488cedf33fa
Vadim Godunko <vgodunko@gmail.com>
2013-01-18 19:39:15 +0000

    Update copyright year.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@3653 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- source/league/league-strings.ads
+++ source/league/league-strings.ads
@@ -8,7 +8,7 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2009-2012, Vadim Godunko <vgodunko@gmail.com>                --
+-- Copyright © 2009-2013, Vadim Godunko <vgodunko@gmail.com>                --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
