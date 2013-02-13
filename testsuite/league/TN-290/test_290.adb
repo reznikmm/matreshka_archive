1f95e9e5b56fce998fc76b1a687da11a01d030d3
Vadim Godunko <vgodunko@gmail.com>
2013-02-13 18:07:21 +0000

    Add automated test.
    Refs #290.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@3773 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- testsuite/league/TN-290/test_290.adb
+++ testsuite/league/TN-290/test_290.adb
@@ -4,11 +4,11 @@
 --                                                                          --
 --         Localization, Internationalization, Globalization for Ada        --
 --                                                                          --
---                              Tools Component                             --
+--                            Testsuite Component                           --
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2009-2013, Vadim Godunko <vgodunko@gmail.com>                --
+-- Copyright © 2013, Vadim Godunko <vgodunko@gmail.com>                     --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
@@ -41,55 +41,22 @@
 ------------------------------------------------------------------------------
 --  $Revision$ $Date$
 ------------------------------------------------------------------------------
-with "matreshka_common.gpr";
-with "matreshka_league.gpr";
+--  Check whether Replace operation doesn't result in unexpected exception.
+------------------------------------------------------------------------------
+with League.Strings;
 
-project Matreshka_League_Tests is
+procedure Test_290 is
+   use type League.Strings.Universal_String;
 
-   for Main use
-    ("base64_test.adb",
-     "string_hash_test.adb",
-     "character_cursor_test.adb",
-     "grapheme_cluster_cursor_test.adb",
-     "case_conversion_test.adb",
-     "case_folding_test.adb",
-     "normalization_test.adb",
-     "additional_normalization_test.adb",
-     "collation_test.adb",
-     "string_performance.adb",
-     "fill_null_terminator_performance.adb",
-     "string_operations_test.adb",
-     "string_compare_test.adb",
-     "library_level_test.adb",
-     "regexp_ataresearch.adb",
-     "test_35.adb",
-     "test_104.adb",
-     "test_106.adb",
-     "test_139.adb",
-     "test_150.adb",
-     "test_165.adb",
-     "test_177.adb",
-     "test_193.adb",
-     "test_209.adb",
-     "test_290.adb",
-     "arguments_environment_test.adb");
-   for Object_Dir use "../.objs";
-   for Source_Dirs use
-    ("../testsuite/league",
-     "../testsuite/league/TN-35",
-     "../testsuite/league/TN-104",
-     "../testsuite/league/TN-106",
-     "../testsuite/league/TN-139",
-     "../testsuite/league/TN-150",
-     "../testsuite/league/TN-165",
-     "../testsuite/league/TN-177",
-     "../testsuite/league/TN-193",
-     "../testsuite/league/TN-209",
-     "../testsuite/league/TN-290",
-     "../tools");
+   S : League.Strings.Universal_String
+     := League.Strings.To_Universal_String ("123");
+   E : League.Strings.Universal_String
+     := League.Strings.To_Universal_String ("13");
 
-   package Compiler is
-      for Default_Switches ("Ada") use Matreshka_Common.Common_Ada_Switches;
-   end Compiler;
+begin
+   S.Replace (2, 2, League.Strings.Empty_Universal_String);
 
-end Matreshka_League_Tests;
+   if S /= E then
+      raise Program_Error;
+   end if;
+end Test_290;
