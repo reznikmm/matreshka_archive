7d0d4dd4039f6df5ca3ebd840ab7f78638b3fb77
Vadim Godunko <vgodunko@gmail.com>
2013-03-26 17:09:21 +0000

    Explicitly specify alignment for vector types to avoid new behavior of GNAT.
    Refs #298.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@3822 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- source/league/arch/x86_generic/matreshka-internals-simd-intel.ads
+++ source/league/arch/x86_generic/matreshka-internals-simd-intel.ads
@@ -58,70 +58,86 @@ package Matreshka.Internals.SIMD.Intel is
    --  256-bit types
 
    type v32qi is array (1 .. 32) of Interfaces.Integer_8;
+   for v32qi'Alignment use 32;
    pragma Machine_Attribute (v32qi, "vector_type");
    pragma Machine_Attribute (v32qi, "may_alias");
 
    type v16hi is array (1 .. 16) of Interfaces.Integer_16;
+   for v16hi'Alignment use 32;
    pragma Machine_Attribute (v16hi, "vector_type");
    pragma Machine_Attribute (v16hi, "may_alias");
 
    type v8si is array (1 .. 8) of Interfaces.Integer_32;
+   for v8si'Alignment use 32;
    pragma Machine_Attribute (v8si, "vector_type");
    pragma Machine_Attribute (v8si, "may_alias");
 
    type v4di is array (1 .. 4) of Interfaces.Integer_64;
+   for v4di'Alignment use 32;
    pragma Machine_Attribute (v4di, "vector_type");
    pragma Machine_Attribute (v4di, "may_alias");
 
    type v8sf is array (1 .. 8) of Interfaces.IEEE_Float_32;
+   for v8sf'Alignment use 32;
    pragma Machine_Attribute (v8sf, "vector_type");
    pragma Machine_Attribute (v8sf, "may_alias");
 
    type v4df is array (1 .. 4) of Interfaces.IEEE_Float_64;
+   for v4df'Alignment use 32;
    pragma Machine_Attribute (v4df, "vector_type");
    pragma Machine_Attribute (v4df, "may_alias");
 
    --  128-bit types
 
    type v16qi is array (1 .. 16) of Interfaces.Integer_8;
+   for v16qi'Alignment use 16;
    pragma Machine_Attribute (v16qi, "vector_type");
    pragma Machine_Attribute (v16qi, "may_alias");
 
    type v8hi is array (1 .. 8) of Interfaces.Integer_16;
+   for v8hi'Alignment use 16;
    pragma Machine_Attribute (v8hi, "vector_type");
    pragma Machine_Attribute (v8hi, "may_alias");
 
    type v4si is array (1 .. 4) of Interfaces.Integer_32;
+   for v4si'Alignment use 16;
    pragma Machine_Attribute (v4si, "vector_type");
    pragma Machine_Attribute (v4si, "may_alias");
 
    type v2di is array (1 .. 2) of Interfaces.Integer_64;
+   for v2di'Alignment use 16;
    pragma Machine_Attribute (v2di, "vector_type");
    pragma Machine_Attribute (v2di, "may_alias");
 
    type v4sf is array (1 .. 4) of Interfaces.IEEE_Float_32;
+   for v4sf'Alignment use 16;
    pragma Machine_Attribute (v4sf, "vector_type");
    pragma Machine_Attribute (v4sf, "may_alias");
 
    type v2df is array (1 .. 2) of Interfaces.IEEE_Float_64;
+   for v2df'Alignment use 16;
    pragma Machine_Attribute (v2df, "vector_type");
    pragma Machine_Attribute (v2df, "may_alias");
 
    --  64-bit types
 
    type v8qi is array (1 .. 8) of Interfaces.Integer_8;
+   for v8qi'Alignment use 8;
    pragma Machine_Attribute (v8qi, "vector_type");
    pragma Machine_Attribute (v8qi, "may_alias");
 
    type v4hi is array (1 .. 4) of Interfaces.Integer_16;
+   for v4hi'Alignment use 8;
    pragma Machine_Attribute (v4hi, "vector_type");
    pragma Machine_Attribute (v4hi, "may_alias");
 
    type v2si is array (1 .. 2) of Interfaces.Integer_32;
+   for v2si'Alignment use 8;
    pragma Machine_Attribute (v2si, "vector_type");
    pragma Machine_Attribute (v2si, "may_alias");
 
    type v1di is array (1 .. 1) of Interfaces.Integer_64;
+   for v1di'Alignment use 8;
    pragma Machine_Attribute (v1di, "vector_type");
    pragma Machine_Attribute (v1di, "may_alias");
 
