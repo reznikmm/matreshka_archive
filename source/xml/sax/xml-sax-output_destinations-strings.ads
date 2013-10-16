88d8c70e83b9383d4668f9aa9f83f01b7751a5df
Vadim Godunko <vgodunko@gmail.com>
2013-10-16 06:25:29 +0000

    Add output destination to obtain generated XML document as universal string.
    Refs #69.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@4133 661adc5f-87f8-0310-85d6-9a7b5f58d823

---
+++ source/xml/sax/xml-sax-output_destinations-strings.ads
@@ -0,0 +1,85 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               XML Processor                              --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2013, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+
+package XML.SAX.Output_Destinations.Strings is
+
+   pragma Preelaborate;
+
+   type SAX_String_Output_Destination is
+     limited new XML.SAX.Output_Destinations.SAX_Output_Destination with private;
+
+   type SAX_Output_Destination is limited interface;
+
+   function Get_Text
+    (Self : SAX_String_Output_Destination)
+       return League.Strings.Universal_String;
+   --  Returns accumulated text.
+
+   overriding function Get_Encoding
+    (Self : SAX_String_Output_Destination)
+       return League.Strings.Universal_String;
+   --  Returns character encoding used to convert Unicode character code point
+   --  into the external items.
+
+   overriding procedure Put
+    (Self : in out SAX_String_Output_Destination;
+     Text : League.Strings.Universal_String);
+   overriding procedure Put
+    (Self : in out SAX_String_Output_Destination;
+     Text : Wide_Wide_String);
+   overriding procedure Put
+    (Self : in out SAX_String_Output_Destination;
+     Char : League.Characters.Universal_Character);
+   overriding procedure Put
+    (Self : in out SAX_String_Output_Destination;
+     Char : Wide_Wide_Character);
+
+private
+
+   type SAX_String_Output_Destination is
+     limited new XML.SAX.Output_Destinations.SAX_Output_Destination with record
+      Text : League.Strings.Universal_String;
+   end record;
+
+end XML.SAX.Output_Destinations.Strings;
