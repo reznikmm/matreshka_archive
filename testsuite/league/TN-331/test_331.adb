fe82c0757e5bf4bfbd459ec5cd65d2a81d66b62e
Vadim Godunko <vgodunko@gmail.com>
2013-11-24 15:50:11 +0000

    Add automated test.
    Refs #331.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@4300 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- testsuite/league/TN-331/test_331.adb
+++ testsuite/league/TN-331/test_331.adb
@@ -4,11 +4,11 @@
 --                                                                          --
 --         Localization, Internationalization, Globalization for Ada        --
 --                                                                          --
---                              Tools Component                             --
+--                            Testsuite Component                           --
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2009-2013, Vadim Godunko <vgodunko@gmail.com>                --
+-- Copyright © 2013, Vadim Godunko <vgodunko@gmail.com>                     --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
@@ -41,65 +41,22 @@
 ------------------------------------------------------------------------------
 --  $Revision$ $Date$
 ------------------------------------------------------------------------------
-with "matreshka_common.gpr";
-with "matreshka_league.gpr";
+--  Checkh whether finalization of JSON_Object and JSON_Value doesn't result in
+--  memory corruption.
+------------------------------------------------------------------------------
+with League.JSON.Objects;
+with League.JSON.Values;
+with League.Strings;
 
-project Matreshka_League_Tests is
+procedure Test_331 is
+   Object : League.JSON.Objects.JSON_Object;
+   Value  : League.JSON.Values.JSON_Value;
 
-   for Main use
-    ("base64_test.adb",
-     "string_hash_test.adb",
-     "character_cursor_test.adb",
-     "grapheme_cluster_cursor_test.adb",
-     "case_conversion_test.adb",
-     "case_folding_test.adb",
-     "normalization_test.adb",
-     "additional_normalization_test.adb",
-     "collation_test.adb",
-     "string_performance.adb",
-     "fill_null_terminator_performance.adb",
-     "string_operations_test.adb",
-     "string_compare_test.adb",
-     "library_level_test.adb",
-     "regexp_ataresearch.adb",
-     "test_35.adb",
-     "test_104.adb",
-     "test_106.adb",
-     "test_139.adb",
-     "test_150.adb",
-     "test_165.adb",
-     "test_177.adb",
-     "test_193.adb",
-     "test_209.adb",
-     "test_290.adb",
-     "test_308.adb",
-     "test_315.adb",
-     "test_322.adb",
-     "test_324.adb",
-     "test_331.adb",
-     "arguments_environment_test.adb");
-   for Object_Dir use "../.objs";
-   for Source_Dirs use
-    ("../testsuite/league",
-     "../testsuite/league/TN-35",
-     "../testsuite/league/TN-104",
-     "../testsuite/league/TN-106",
-     "../testsuite/league/TN-139",
-     "../testsuite/league/TN-150",
-     "../testsuite/league/TN-165",
-     "../testsuite/league/TN-177",
-     "../testsuite/league/TN-193",
-     "../testsuite/league/TN-209",
-     "../testsuite/league/TN-290",
-     "../testsuite/league/TN-308",
-     "../testsuite/league/TN-315",
-     "../testsuite/league/TN-322",
-     "../testsuite/league/TN-324",
-     "../testsuite/league/TN-331",
-     "../tools");
+begin
+   Object.Insert
+    (League.Strings.To_Universal_String ("name"),
+     League.JSON.Values.To_JSON_Value
+      (League.Strings.To_Universal_String ("peter")));
 
-   package Compiler is
-      for Default_Switches ("Ada") use Matreshka_Common.Common_Ada_Switches;
-   end Compiler;
-
-end Matreshka_League_Tests;
+   Value := Object.Value (League.Strings.To_Universal_String ("name"));
+end Test_331;
