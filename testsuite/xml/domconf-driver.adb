4db113035e16849a7d84e3cf9e23cf46d58aa2c0
Vadim Godunko <vgodunko@gmail.com>
2014-03-29 09:28:33 +0000

    Move DOMConf driver to main source tree.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@4786 661adc5f-87f8-0310-85d6-9a7b5f58d823

---
+++ testsuite/xml/domconf-driver.adb
@@ -0,0 +1,61 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               XML Processor                              --
+--                                                                          --
+--                            Testsuite Component                           --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2014, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+with League.Application;
+with XML.SAX.Constants;
+with XML.SAX.File_Input_Sources;
+with XML.SAX.Simple_Readers;
+
+with DOMConf.Test_Parsers;
+
+procedure DOMConf.Driver is
+   Input  : aliased XML.SAX.File_Input_Sources.File_Input_Source;
+   Parser : aliased DOMConf.Test_Parsers.Test_Parser;
+   Reader : aliased XML.SAX.Simple_Readers.Simple_Reader;
+
+begin
+   Input.Open_By_File_Name (League.Application.Arguments.Element (1));
+   Reader.Set_Feature (XML.SAX.Constants.Load_External_DTD_Feature, False);
+   Reader.Set_Content_Handler (Parser'Unchecked_Access);
+   Reader.Parse (Input'Unchecked_Access);
+end DOMConf.Driver;
