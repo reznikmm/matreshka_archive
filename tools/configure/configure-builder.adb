e8598d83311d6cf0c2311028c27aea9f5a2ae2d0
Vadim Godunko <vgodunko@gmail.com>
2014-09-11 09:14:42 +0000

    Report executed command line and its output in all cases.
    Remove code duplication.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@4873 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- tools/configure/configure-builder.adb
+++ tools/configure/configure-builder.adb
@@ -42,13 +42,81 @@
 --  $Revision$ $Date$
 ------------------------------------------------------------------------------
 with Ada.Directories;
+with Ada.Text_IO;
 with GNAT.Expect;
+with GNAT.OS_Lib;
 
 with Configure.Instantiate;
+with Configure.Internals;
 
 package body Configure.Builder is
 
    use GNAT.Expect;
+
+   function To_String (Args : GNAT.OS_Lib.Argument_List) return String;
+   --  Converts argument list into printable string.
+
+   function Build
+    (Report_Log : not null access procedure (Message : String);
+     Directory  : String) return Boolean;
+   --  Common code for both variants of procedure.
+
+   -----------
+   -- Build --
+   -----------
+
+   function Build
+    (Report_Log : not null access procedure (Message : String);
+     Directory  : String) return Boolean
+   is
+      Args   : constant GNAT.OS_Lib.Argument_List
+        := (1 => new String'("-p"),
+            2 => new String'("-P" & Directory & "/check.gpr"));
+      Result : Boolean;
+
+   begin
+      --  Generate project file from template when necessary.
+
+      if Ada.Directories.Exists (Directory & "check.gpr.in") then
+         Configure.Instantiate (Directory & "check.gpr", True);
+      end if;
+
+      --  Run builder.
+
+      Report_Log ("gnatmake" & To_String (Args));
+
+      begin
+         declare
+            Status : aliased Integer;
+            Output : constant String :=
+              Get_Command_Output ("gnatmake", Args, "", Status'Access, True);
+
+         begin
+            Report_Log (Output);
+            Result := Status = 0;
+         end;
+
+      exception
+         when GNAT.Expect.Invalid_Process =>
+            Result := False;
+      end;
+
+      --  Cleanup build directory.
+
+      if Ada.Directories.Exists (Directory & "_build") then
+         Ada.Directories.Delete_Tree (Directory & "_build");
+      end if;
+
+      --  Remove generated project file when necessary.
+
+      if Ada.Directories.Exists (Directory & "check.gpr.in")
+        and Ada.Directories.Exists (Directory & "check.gpr")
+      then
+         Ada.Directories.Delete_File (Directory & "check.gpr");
+      end if;
+
+      return Result;
+   end Build;
 
    -----------
    -- Build --
@@ -58,54 +126,21 @@ package body Configure.Builder is
     (Test      : Configure.Abstract_Tests.Abstract_Test'Class;
      Directory : String) return Boolean
    is
-      Result : Boolean;
+
+      procedure Report_Log (Message : String);
+      --  Reports message using test's log stream.
+
+      ----------------
+      -- Report_Log --
+      ----------------
+
+      procedure Report_Log (Message : String) is
+      begin
+         Test.Report_Log (Message);
+      end Report_Log;
 
    begin
-      --  Generate project file from template when necessary.
-
-      if Ada.Directories.Exists (Directory & "check.gpr.in") then
-         Configure.Instantiate (Directory & "check.gpr", True);
-      end if;
-
-      --  Run builder.
-
-      begin
-         declare
-            Status : aliased Integer;
-            Output : constant String :=
-              Get_Command_Output
-               ("gnatmake",
-                (1 => new String'("-p"),
-                 2 => new String'("-P" & Directory & "/check.gpr")),
-                "",
-                Status'Access,
-                True);
-
-         begin
-            Test.Report_Log (Output);
-            Result := Status = 0;
-         end;
-
-      exception
-         when GNAT.Expect.Invalid_Process =>
-            Result := False;
-      end;
-
-      --  Cleanup build directory.
-
-      if Ada.Directories.Exists (Directory & "_build") then
-         Ada.Directories.Delete_Tree (Directory & "_build");
-      end if;
-
-      --  Remove generated project file when necessary.
-
-      if Ada.Directories.Exists (Directory & "check.gpr.in")
-        and Ada.Directories.Exists (Directory & "check.gpr")
-      then
-         Ada.Directories.Delete_File (Directory & "check.gpr");
-      end if;
-
-      return Result;
+      return Build (Report_Log'Access, Directory);
    end Build;
 
    -----------
@@ -113,53 +148,38 @@ package body Configure.Builder is
    -----------
 
    function Build (Directory : String) return Boolean is
-      Result : Boolean;
+
+      procedure Report_Log (Message : String);
+      --  Reports message using global log stream.
+
+      ----------------
+      -- Report_Log --
+      ----------------
+
+      procedure Report_Log (Message : String) is
+      begin
+         Ada.Text_IO.Put_Line (Configure.Internals.Log_Output, Message);
+         Ada.Text_IO.Flush (Configure.Internals.Log_Output);
+      end Report_Log;
 
    begin
-      --  Generate project file from template when necessary.
-
-      if Ada.Directories.Exists (Directory & "check.gpr.in") then
-         Configure.Instantiate (Directory & "check.gpr", True);
-      end if;
-
-      --  Run builder.
-
-      begin
-         declare
-            Status : aliased Integer;
-            Output : constant String :=
-              Get_Command_Output
-               ("gnatmake",
-                (1 => new String'("-p"),
-                 2 => new String'("-P" & Directory & "/check.gpr")),
-                "",
-                Status'Access,
-                True);
-
-         begin
-            Result := Status = 0;
-         end;
-
-      exception
-         when GNAT.Expect.Invalid_Process =>
-            Result := False;
-      end;
-
-      --  Cleanup build directory.
-
-      if Ada.Directories.Exists (Directory & "_build") then
-         Ada.Directories.Delete_Tree (Directory & "_build");
-      end if;
-
-      --  Remove generated project file when necessary.
-
-      if Ada.Directories.Exists (Directory & "check.gpr.in")
-        and Ada.Directories.Exists (Directory & "check.gpr")
-      then
-         Ada.Directories.Delete_File (Directory & "check.gpr");
-      end if;
-
-      return Result;
+      return Build (Report_Log'Access, Directory);
    end Build;
+
+   ---------------
+   -- To_String --
+   ---------------
+
+   function To_String (Args : GNAT.OS_Lib.Argument_List) return String is
+      Result : Unbounded_String;
+
+   begin
+      for Index in Args'Range loop
+         Append (Result, ' ');
+         Append (Result, Args (Index).all);
+      end loop;
+
+      return To_String (Result);
+   end To_String;
 
 end Configure.Builder;
