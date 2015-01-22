419daa7ee2d888f685cb956c6af2002d3f0e1ff4
Vadim Godunko <vgodunko@gmail.com>
2015-01-22 07:53:52 +0000

    Move container initializer into primary source tree.
    Refs #395.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@5178 661adc5f-87f8-0310-85d6-9a7b5f58d823

---
+++ source/web/servlet/servlet-container_initializers.ads
@@ -0,0 +1,66 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               Web Framework                              --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2014-2015, Vadim Godunko <vgodunko@gmail.com>                --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+--  Interface which allows a library/runtime to be notified of a web
+--  application's startup phase and perform any required programmatic
+--  registration of servlets, filters, and listeners in response to it.
+------------------------------------------------------------------------------
+with Servlet.Contexts;
+
+package Servlet.Container_Initializers is
+
+   pragma Preelaborate;
+
+   type Servlet_Container_Initializer is limited interface;
+
+   type Servlet_Container_Initializer_Access is
+     access all Servlet_Container_Initializer'Class;
+
+   not overriding procedure On_Startup
+    (Self    : in out Servlet_Container_Initializer;
+     Context : in out Servlet.Contexts.Servlet_Context'Class)
+       is abstract;
+   --  Notifies this ServletContainerInitializer of the startup of the
+   --  application represented by the given ServletContext.
+
+end Servlet.Container_Initializers;
