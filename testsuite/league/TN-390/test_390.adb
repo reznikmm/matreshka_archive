5df272412e381649c7800a601493ee3b4b3dbc58
Vadim Godunko <vgodunko@gmail.com>
2015-05-22 08:33:36 +0000

    Add automated test.
    Refs #390.
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@5329 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- testsuite/league/TN-390/test_390.adb
+++ testsuite/league/TN-390/test_390.adb
@@ -4,11 +4,11 @@
 --                                                                          --
 --         Localization, Internationalization, Globalization for Ada        --
 --                                                                          --
---                              Tools Component                             --
+--                            Testsuite Component                           --
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2009-2015, Vadim Godunko <vgodunko@gmail.com>                --
+-- Copyright © 2015, Vadim Godunko <vgodunko@gmail.com>                     --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
@@ -41,77 +41,17 @@
 ------------------------------------------------------------------------------
 --  $Revision$ $Date$
 ------------------------------------------------------------------------------
-with "matreshka_common.gpr";
-with "matreshka_league.gpr";
+--  Check that Universal_String.Index doesn't raise exception when pattern
+--  string is longer then string to lookup in.
+------------------------------------------------------------------------------
+with League.Strings;
 
-project Matreshka_League_Tests is
+procedure Test_390 is
+   X : League.Strings.Universal_String
+     := League.Strings.To_Universal_String ("ENV_IN");
 
-   for Main use
-    ("base64_test.adb",
-     "string_hash_test.adb",
-     "character_cursor_test.adb",
-     "grapheme_cluster_cursor_test.adb",
-     "case_conversion_test.adb",
-     "case_folding_test.adb",
-     "normalization_test.adb",
-     "additional_normalization_test.adb",
-     "collation_test.adb",
-     "string_performance.adb",
-     "fill_null_terminator_performance.adb",
-     "string_operations_test.adb",
-     "string_compare_test.adb",
-     "library_level_test.adb",
-     "regexp_ataresearch.adb",
-     "test_7.adb",
-     "test_35.adb",
-     "test_104.adb",
-     "test_106.adb",
-     "test_139.adb",
-     "test_150.adb",
-     "test_165.adb",
-     "test_177.adb",
-     "test_193.adb",
-     "test_209.adb",
-     "test_290.adb",
-     "test_308.adb",
-     "test_315.adb",
-     "test_322.adb",
-     "test_324.adb",
-     "test_331.adb",
-     "test_333.adb",
-     "test_368.adb",
-     "test_379.adb",
-     "test_388.adb",
-     "test_390.adb",
-     "arguments_environment_test.adb");
-   for Object_Dir use "../.objs";
-   for Source_Dirs use
-    ("../testsuite/league",
-     "../testsuite/league/TN-7",
-     "../testsuite/league/TN-35",
-     "../testsuite/league/TN-104",
-     "../testsuite/league/TN-106",
-     "../testsuite/league/TN-139",
-     "../testsuite/league/TN-150",
-     "../testsuite/league/TN-165",
-     "../testsuite/league/TN-177",
-     "../testsuite/league/TN-193",
-     "../testsuite/league/TN-209",
-     "../testsuite/league/TN-290",
-     "../testsuite/league/TN-308",
-     "../testsuite/league/TN-315",
-     "../testsuite/league/TN-322",
-     "../testsuite/league/TN-324",
-     "../testsuite/league/TN-331",
-     "../testsuite/league/TN-333",
-     "../testsuite/league/TN-368",
-     "../testsuite/league/TN-379",
-     "../testsuite/league/TN-388",
-     "../testsuite/league/TN-390",
-     "../tools");
-
-   package Compiler is
-      for Default_Switches ("Ada") use Matreshka_Common.Common_Ada_Switches;
-   end Compiler;
-
-end Matreshka_League_Tests;
+begin
+   if X.Index ("ENV_OUT") /= 0 then
+      raise Program_Error;
+   end if;
+end Test_390;
