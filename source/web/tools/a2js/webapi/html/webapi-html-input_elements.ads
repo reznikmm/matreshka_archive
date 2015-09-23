4d2a3ff8a86c89c5adea5c7f5164f1fdc8d32594
Vadim Godunko <vgodunko@gmail.com>
2015-09-23 08:25:22 +0000

    Initial version of HTML_Input_Element.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@5460 661adc5f-87f8-0310-85d6-9a7b5f58d823

---
+++ source/web/tools/a2js/webapi/html/webapi-html-input_elements.ads
@@ -0,0 +1,240 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               Web Framework                              --
+--                                                                          --
+--                            Web API Definition                            --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2014-2015, Vadim Godunko <vgodunko@gmail.com>                --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+with League.Strings;
+
+with WebAPI.HTML.Elements;
+
+package WebAPI.HTML.Input_Elements is
+
+   pragma Preelaborate;
+
+   type HTML_Input_Element is limited interface
+     and WebAPI.HTML.Elements.HTML_Element;
+
+   type HTML_Input_Element_Access is access all HTML_Input_Element'Class
+      with Storage_Size => 0;
+
+   --  interface HTMLInputElement : HTMLElement {
+   --    readonly attribute HTMLFormElement? form;
+   --    readonly attribute FileList? files;
+
+   not overriding function Get_Accept
+    (Self : not null access constant HTML_Input_Element)
+       return League.Strings.Universal_String is abstract
+         with Import     => True,
+              Convention => JavaScript_Property_Getter,
+              Link_Name  => "accept";
+
+   not overriding procedure Set_Accept
+    (Self : not null access constant HTML_Input_Element;
+     To   : League.Strings.Universal_String) is abstract
+       with Import     => True,
+            Convention => JavaScript_Property_Setter,
+            Link_Name  => "accept";
+
+   not overriding function Get_Alt
+    (Self : not null access constant HTML_Input_Element)
+       return League.Strings.Universal_String is abstract
+         with Import     => True,
+              Convention => JavaScript_Property_Getter,
+              Link_Name  => "alt";
+
+   not overriding procedure Set_Alt
+    (Self : not null access constant HTML_Input_Element;
+     To   : League.Strings.Universal_String) is abstract
+       with Import     => True,
+            Convention => JavaScript_Property_Setter,
+            Link_Name  => "alt";
+
+   not overriding function Get_Autocomplete
+    (Self : not null access constant HTML_Input_Element)
+       return League.Strings.Universal_String is abstract
+         with Import     => True,
+              Convention => JavaScript_Property_Getter,
+              Link_Name  => "autocomplete";
+
+   not overriding procedure Set_Autocomplete
+    (Self : not null access constant HTML_Input_Element;
+     To   : League.Strings.Universal_String) is abstract
+       with Import     => True,
+            Convention => JavaScript_Property_Setter,
+            Link_Name  => "autocomplete";
+
+   not overriding function Get_Autofocus
+    (Self : not null access constant HTML_Input_Element)
+       return Boolean is abstract
+         with Import     => True,
+              Convention => JavaScript_Property_Getter,
+              Link_Name  => "autofocus";
+
+   not overriding procedure Set_Autofocus
+    (Self : not null access constant HTML_Input_Element;
+     To   : Boolean) is abstract
+       with Import     => True,
+            Convention => JavaScript_Property_Setter,
+            Link_Name  => "autofocus";
+
+   not overriding function Get_Default_Checked
+    (Self : not null access constant HTML_Input_Element)
+       return Boolean is abstract
+         with Import     => True,
+              Convention => JavaScript_Property_Getter,
+              Link_Name  => "defaultChecked";
+
+   not overriding procedure Set_Default_Checked
+    (Self : not null access constant HTML_Input_Element;
+     To   : Boolean) is abstract
+       with Import     => True,
+            Convention => JavaScript_Property_Setter,
+            Link_Name  => "defaultChecked";
+
+   not overriding function Get_Checked
+    (Self : not null access constant HTML_Input_Element)
+       return Boolean is abstract
+         with Import     => True,
+              Convention => JavaScript_Property_Getter,
+              Link_Name  => "checked";
+
+   not overriding procedure Set_Checked
+    (Self : not null access constant HTML_Input_Element;
+     To   : Boolean) is abstract
+       with Import     => True,
+            Convention => JavaScript_Property_Setter,
+            Link_Name  => "checked";
+
+   not overriding function Get_Dir_Name
+    (Self : not null access constant HTML_Input_Element)
+       return League.Strings.Universal_String is abstract
+         with Import     => True,
+              Convention => JavaScript_Property_Getter,
+              Link_Name  => "dirName";
+
+   not overriding procedure Set_Dir_Name
+    (Self : not null access constant HTML_Input_Element;
+     To   : League.Strings.Universal_String) is abstract
+       with Import     => True,
+            Convention => JavaScript_Property_Setter,
+            Link_Name  => "dirName";
+
+   not overriding function Get_Disabled
+    (Self : not null access constant HTML_Input_Element)
+       return Boolean is abstract
+         with Import     => True,
+              Convention => JavaScript_Property_Getter,
+              Link_Name  => "disabled";
+
+   not overriding procedure Set_Disabled
+    (Self : not null access constant HTML_Input_Element;
+     To   : Boolean) is abstract
+       with Import     => True,
+            Convention => JavaScript_Property_Setter,
+            Link_Name  => "disabled";
+
+   --  interface HTMLInputElement : HTMLElement {
+   --    readonly attribute HTMLFormElement? form;
+   --    readonly attribute FileList? files;
+
+   --  interface HTMLInputElement : HTMLElement {
+   --             attribute DOMString formAction;
+   --             attribute DOMString formEnctype;
+   --             attribute DOMString formMethod;
+   --             attribute boolean formNoValidate;
+   --             attribute DOMString formTarget;
+   --             attribute unsigned long height;
+   --             attribute boolean indeterminate;
+   --    readonly attribute HTMLElement? list;
+   --             attribute DOMString max;
+   --             attribute long maxLength;
+   --             attribute DOMString min;
+   --             attribute long minLength;
+   --             attribute boolean multiple;
+   --             attribute DOMString name;
+   --             attribute DOMString pattern;
+   --             attribute DOMString placeholder;
+   --             attribute boolean readOnly;
+   --             attribute boolean required;
+   --             attribute unsigned long size;
+   --             attribute DOMString src;
+   --             attribute DOMString step;
+   --             attribute DOMString type;
+   --             attribute DOMString defaultValue;
+   --             attribute Date? valueAsDate;
+   --             attribute unrestricted double valueAsNumber;
+   --             attribute unsigned long width;
+   --
+   --    void stepUp(optional long n = 1);
+   --    void stepDown(optional long n = 1);
+   --
+   --    readonly attribute boolean willValidate;
+   --    readonly attribute ValidityState validity;
+   --    readonly attribute DOMString validationMessage;
+   --    boolean checkValidity();
+   --    void setCustomValidity(DOMString error);
+   --
+   --    readonly attribute NodeList labels;
+   --
+   --    void select();
+   --             attribute unsigned long selectionStart;
+   --             attribute unsigned long selectionEnd;
+   --             attribute DOMString selectionDirection;
+   --    void setRangeText(DOMString replacement);
+   --    void setRangeText(DOMString replacement, unsigned long start, unsigned long end, optional SelectionMode selectionMode = "preserve");
+   --    void setSelectionRange(unsigned long start, unsigned long end, optional DOMString direction);
+   --  };
+
+   not overriding function Get_Value
+    (Self : not null access constant HTML_Input_Element)
+       return League.Strings.Universal_String is abstract
+         with Import     => True,
+              Convention => JavaScript_Property_Getter,
+              Link_Name  => "value";
+
+   not overriding procedure Set_Value
+    (Self : not null access constant HTML_Input_Element;
+     To   : League.Strings.Universal_String) is abstract
+       with Import     => True,
+            Convention => JavaScript_Property_Setter,
+            Link_Name  => "value";
+
+end WebAPI.HTML.Input_Elements;
