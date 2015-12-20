08058e8c9172ce43548ee2a2375d6f4c7fd75bb7
Vadim Godunko <vgodunko@gmail.com>
2015-12-20 11:12:56 +0000

    Update API of Event binding.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@5517 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- source/web/tools/a2js/webapi/dom/webapi-dom-events.ads
+++ source/web/tools/a2js/webapi/dom/webapi-dom-events.ads
@@ -41,7 +41,8 @@
 ------------------------------------------------------------------------------
 --  $Revision$ $Date$
 ------------------------------------------------------------------------------
---  This package provides binding to interface Event.
+--  This package provides binding to interface Event as defined by DOM Level 4
+--  specification.
 ------------------------------------------------------------------------------
 with League.Strings;
 limited with WebAPI.DOM.Event_Targets;
@@ -50,9 +51,15 @@ package WebAPI.DOM.Events is
 
    pragma Preelaborate;
 
+   type Event_Phases is
+    (None,
+     Capturing_Phase,
+     At_Target,
+     Bubbling_Phase);
+
    type Event is limited interface;
 
-   not overriding function Event_Type
+   not overriding function Get_Event_Type
     (Self : not null access constant Event)
        return League.Strings.Universal_String is abstract
          with Import        => True,
@@ -60,7 +67,7 @@ package WebAPI.DOM.Events is
               External_Name => "type";
    --  The name of the event (case-insensitive). The name must be an XML name.
 
-   not overriding function Target
+   not overriding function Get_Target
     (Self : not null access constant Event)
        return WebAPI.DOM.Event_Targets.Event_Target_Access is abstract
          with Import        => True,
@@ -69,7 +76,7 @@ package WebAPI.DOM.Events is
    --  Used to indicate the EventTarget to which the event was originally
    --  dispatched.
 
-   not overriding function Current_Target
+   not overriding function Get_Current_Target
     (Self : not null access constant Event)
        return WebAPI.DOM.Event_Targets.Event_Target_Access is abstract
          with Import        => True,
@@ -78,6 +85,36 @@ package WebAPI.DOM.Events is
    --  Used to indicate the EventTarget whose EventListeners are currently
    --  being processed. This is particularly useful during capturing and
    --  bubbling.
+
+   not overriding function Get_Event_Phase
+    (Self : not null access constant Event)
+       return Event_Phases is abstract
+         with Import        => True,
+              Convention    => JavaScript_Property_Getter,
+              External_Name => "eventPhase";
+   --  Returns the event's phase, which is one of NONE, CAPTURING_PHASE,
+   --  AT_TARGET, and BUBBLING_PHASE.
+
+   not overriding procedure Stop_Propagation
+    (Self : not null access Event) is abstract
+       with Import        => True,
+            Convention    => JavaScript_Method,
+            External_Name => "stopPropagation";
+   --  The stopPropagation method is used prevent further propagation of an
+   --  event during event flow. If this method is called by any EventListener
+   --  the event will cease propagating through the tree. The event will
+   --  complete dispatch to all listeners on the current EventTarget before
+   --  event flow stops. This method may be used during any stage of event
+   --  flow.
+
+   not overriding procedure Stop_Immediate_Propagation
+    (Self : not null access Event) is abstract
+       with Import        => True,
+            Convention    => JavaScript_Method,
+            External_Name => "stopImmediatePropagation()";
+   --  Invoking this method prevents event from reaching any registered event
+   --  listeners after the current one finishes running and, when dispatched in
+   --  a tree, also prevents event from reaching any other objects. 
 
    not overriding function Bubbles
     (Self : not null access constant Event)
@@ -98,18 +135,6 @@ package WebAPI.DOM.Events is
    --  prevented. If the default action can be prevented the value is true,
    --  else the value is false.
 
-   not overriding procedure Stop_Propagation
-    (Self : not null access Event) is abstract
-       with Import        => True,
-            Convention    => JavaScript_Method,
-            External_Name => "stopPropagation";
-   --  The stopPropagation method is used prevent further propagation of an
-   --  event during event flow. If this method is called by any EventListener
-   --  the event will cease propagating through the tree. The event will
-   --  complete dispatch to all listeners on the current EventTarget before
-   --  event flow stops. This method may be used during any stage of event
-   --  flow.
-
    not overriding procedure Prevent_Default
     (Self : not null access Event) is abstract
        with Import        => True,
@@ -124,5 +149,23 @@ package WebAPI.DOM.Events is
    --  effect. Once preventDefault has been called it will remain in effect
    --  throughout the remainder of the event's propagation. This method may be
    --  used during any stage of event flow.
+
+   not overriding function Get_Default_Prevented
+    (Self : not null access constant Event)
+       return Boolean is abstract
+         with Import        => True,
+              Convention    => JavaScript_Property_Getter,
+              External_Name => "defaultPrevented";
+   --  Returns true if preventDefault() was invoked while the cancelable
+   --  attribute value is true, and false otherwise.
+
+   not overriding function Get_Is_Trusted
+    (Self : not null access constant Event)
+       return Boolean is abstract
+         with Import        => True,
+              Convention    => JavaScript_Property_Getter,
+              External_Name => "isTrusted";
+   --  Returns true if event was dispatched by the user agent, and false
+   --  otherwise.
 
 end WebAPI.DOM.Events;
