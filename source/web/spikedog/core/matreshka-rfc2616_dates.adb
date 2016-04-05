faf8a848a887589d501bf02c230d143c6be95907
Maxim Reznik <reznikmm@gmail.com>
2016-04-05 12:01:01 +0000

    Reorder subprograms to fix STORAGE_ERROR
    while compiling on x86 (32-bits) with old compilers
    
    
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@5571 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- source/web/spikedog/core/matreshka-rfc2616_dates.adb
+++ source/web/spikedog/core/matreshka-rfc2616_dates.adb
@@ -61,6 +61,28 @@ package body Matreshka.RFC2616_Dates is
         := +"EEE, dd MMM yyyy HH:mm:ss ";
    end Constants;
 
+   procedure Parse_Time
+     (Text   : League.Strings.Universal_String;
+      Hour   : out League.Calendars.ISO_8601.Hour_Number;
+      Minute : out League.Calendars.ISO_8601.Minute_Number;
+      Second : out League.Calendars.ISO_8601.Second_Number);
+   
+   procedure Parse_ANSI_Date
+     (Text    : League.Strings.Universal_String;
+      Value   : out League.Calendars.Date_Time;
+      Success : out Boolean);
+
+   procedure Parse_RFC_1123_Date
+     (Text    : League.Strings.Universal_String;
+      Value   : out League.Calendars.Date_Time;
+      Success : out Boolean);
+   
+   procedure Parse_RFC_850_Date
+     (Text    : League.Strings.Universal_String;
+      Value   : out League.Calendars.Date_Time;
+      Success : out Boolean);
+   
+   
    -----------------
    -- From_String --
    -----------------
@@ -70,180 +92,183 @@ package body Matreshka.RFC2616_Dates is
       Value   : out League.Calendars.Date_Time;
       Success : out Boolean)
    is
-      use type League.Strings.Universal_String;
-
-      procedure Parse_RFC_1123_Date;
-      procedure Parse_ANSI_Date;
-      procedure Parse_RFC_850_Date;
-
-      procedure Parse_Time
-        (Text   : League.Strings.Universal_String;
-         Hour   : out League.Calendars.ISO_8601.Hour_Number;
-         Minute : out League.Calendars.ISO_8601.Minute_Number;
-         Second : out League.Calendars.ISO_8601.Second_Number);
-
-      ---------------------
-      -- Parse_ANSI_Date --
-      ---------------------
-
-      procedure Parse_ANSI_Date is
-         use League.Calendars.ISO_8601;
-
-         List : constant League.String_Vectors.Universal_String_Vector :=
-           Text.Split (' ', League.Strings.Skip_Empty);
-
-         Year   : Year_Number;
-         Month  : Month_Number;
-         Day    : Day_Number;
-         Hour   : Hour_Number;
-         Minute : Minute_Number;
-         Second : Second_Number;
-      begin
-         --  Text example: Sun Nov  6 08:49:37 1994
-         Year := Year_Number'Wide_Wide_Value
-           (List.Element (5).To_Wide_Wide_String);
-
-         Month := Month_Number
-           (Constants.Month_List.Index (List.Element (2)) / 3);
-
-         Day := Day_Number'Wide_Wide_Value
-           (List.Element (3).To_Wide_Wide_String);
-
-         Parse_Time (List.Element (4), Hour, Minute, Second);
-
-         Value := Create (Year, Month, Day, Hour, Minute, Second, 0);
-
-         Success := True;
-
-      exception
-         when Constraint_Error =>
-            Success := False;
-      end Parse_ANSI_Date;
-
-      -------------------------
-      -- Parse_RFC_1123_Date --
-      -------------------------
-
-      procedure Parse_RFC_1123_Date is
-         use League.Calendars.ISO_8601;
-
-         List : constant League.String_Vectors.Universal_String_Vector :=
-           Text.Split (' ');
-
-         Year   : Year_Number;
-         Month  : Month_Number;
-         Day    : Day_Number;
-         Hour   : Hour_Number;
-         Minute : Minute_Number;
-         Second : Second_Number;
-      begin
-         --  Text example: Sun, 06 Nov 1994 08:49:37 GMT
-         Year := Year_Number'Wide_Wide_Value
-           (List.Element (4).To_Wide_Wide_String);
-
-         Month := Month_Number
-           (Constants.Month_List.Index (List.Element (3)) / 3);
-
-         Day := Day_Number'Wide_Wide_Value
-           (List.Element (2).To_Wide_Wide_String);
-
-         Success := List.Element (6) = Constants.GMT;
-
-         Parse_Time (List.Element (5), Hour, Minute, Second);
-
-         Value := Create (Year, Month, Day, Hour, Minute, Second, 0);
-
-      exception
-         when Constraint_Error =>
-            Success := False;
-      end Parse_RFC_1123_Date;
-
-      ------------------------
-      -- Parse_RFC_850_Date --
-      ------------------------
-
-      procedure Parse_RFC_850_Date is
-         use League.Calendars.ISO_8601;
-
-         List : constant League.String_Vectors.Universal_String_Vector :=
-           Text.Split (' ');
-
-         Date   : League.Strings.Universal_String;
-         Year   : Year_Number;
-         Month  : Month_Number;
-         Day    : Day_Number;
-         Hour   : Hour_Number;
-         Minute : Minute_Number;
-         Second : Second_Number;
-      begin
-         --  Text example: Sunday, 06-Nov-94 08:49:37 GMT
-         Date := List.Element (2);
-
-         Year := Year_Number'Wide_Wide_Value
-           (Date.Tail_From (8).To_Wide_Wide_String);
-
-         if Year in 0 .. 49 then
-            Year := Year + 2000;
-         else
-            Year := Year + 1900;
-         end if;
-
-         Month := Month_Number
-           (Constants.Month_List.Index (Date.Slice (4, 6)) / 3);
-
-         Day := Day_Number'Wide_Wide_Value
-           (Date.Head_To (2).To_Wide_Wide_String);
-
-         Success := List.Element (4) = Constants.GMT;
-
-         Parse_Time (List.Element (3), Hour, Minute, Second);
-
-         Value := Create (Year, Month, Day, Hour, Minute, Second, 0);
-
-      exception
-         when Constraint_Error =>
-            Success := False;
-      end Parse_RFC_850_Date;
-
-      ----------------
-      -- Parse_Time --
-      ----------------
-
-      procedure Parse_Time
-        (Text   : League.Strings.Universal_String;
-         Hour   : out League.Calendars.ISO_8601.Hour_Number;
-         Minute : out League.Calendars.ISO_8601.Minute_Number;
-         Second : out League.Calendars.ISO_8601.Second_Number)
-      is
-         use League.Calendars.ISO_8601;
-
-         Time : constant League.String_Vectors.Universal_String_Vector
-           := Text.Split (':');
-      begin
-         Hour := Hour_Number'Wide_Wide_Value
-           (Time.Element (1).To_Wide_Wide_String);
-
-         Minute := Minute_Number'Wide_Wide_Value
-           (Time.Element (2).To_Wide_Wide_String);
-
-         Second := Second_Number'Wide_Wide_Value
-           (Time.Element (3).To_Wide_Wide_String);
-      end Parse_Time;
-
       Char_4 : constant Wide_Wide_Character
         := Text.Element (4).To_Wide_Wide_Character;
 
    begin
-      case Char_4 is
-         when ',' =>
-            Parse_RFC_1123_Date;
-         when ' ' =>
-            Parse_ANSI_Date;
-         when others =>
-            Parse_RFC_850_Date;
-      end case;
+      if Char_4 = ',' then
+         Parse_RFC_1123_Date (Text, Value, Success);
+      elsif Char_4 = ' ' then
+         Parse_ANSI_Date (Text, Value, Success);
+      else
+         Parse_RFC_850_Date (Text, Value, Success);
+      end if;
    end From_String;
+   
+   ---------------------
+   -- Parse_ANSI_Date --
+   ---------------------
 
+   procedure Parse_ANSI_Date
+     (Text    : League.Strings.Universal_String;
+      Value   : out League.Calendars.Date_Time;
+      Success : out Boolean)
+   is
+      use League.Calendars.ISO_8601;
+
+      List : constant League.String_Vectors.Universal_String_Vector :=
+        Text.Split (' ', League.Strings.Skip_Empty);
+
+      Year   : Year_Number;
+      Month  : Month_Number;
+      Day    : Day_Number;
+      Hour   : Hour_Number;
+      Minute : Minute_Number;
+      Second : Second_Number;
+   begin
+      --  Text example: Sun Nov  6 08:49:37 1994
+      Year := Year_Number'Wide_Wide_Value
+        (List.Element (5).To_Wide_Wide_String);
+
+      Month := Month_Number
+        (Constants.Month_List.Index (List.Element (2)) / 3);
+
+      Day := Day_Number'Wide_Wide_Value
+        (List.Element (3).To_Wide_Wide_String);
+
+      Parse_Time (List.Element (4), Hour, Minute, Second);
+
+      Value := Create (Year, Month, Day, Hour, Minute, Second, 0);
+
+      Success := True;
+
+   exception
+      when Constraint_Error =>
+         Success := False;
+   end Parse_ANSI_Date;
+   
+   -------------------------
+   -- Parse_RFC_1123_Date --
+   -------------------------
+
+   procedure Parse_RFC_1123_Date
+     (Text    : League.Strings.Universal_String;
+      Value   : out League.Calendars.Date_Time;
+      Success : out Boolean)
+   is
+      use League.Calendars.ISO_8601;
+      use type League.Strings.Universal_String;
+
+      List : constant League.String_Vectors.Universal_String_Vector :=
+        Text.Split (' ');
+
+      Year   : Year_Number;
+      Month  : Month_Number;
+      Day    : Day_Number;
+      Hour   : Hour_Number;
+      Minute : Minute_Number;
+      Second : Second_Number;
+   begin
+      --  Text example: Sun, 06 Nov 1994 08:49:37 GMT
+      Year := Year_Number'Wide_Wide_Value
+        (List.Element (4).To_Wide_Wide_String);
+
+      Month := Month_Number
+        (Constants.Month_List.Index (List.Element (3)) / 3);
+
+      Day := Day_Number'Wide_Wide_Value
+        (List.Element (2).To_Wide_Wide_String);
+
+      Success := List.Element (6) = Constants.GMT;
+
+      Parse_Time (List.Element (5), Hour, Minute, Second);
+
+      Value := Create (Year, Month, Day, Hour, Minute, Second, 0);
+
+   exception
+      when Constraint_Error =>
+         Success := False;
+   end Parse_RFC_1123_Date;
+
+   ------------------------
+   -- Parse_RFC_850_Date --
+   ------------------------
+
+   procedure Parse_RFC_850_Date
+     (Text    : League.Strings.Universal_String;
+      Value   : out League.Calendars.Date_Time;
+      Success : out Boolean)
+   is
+      use League.Calendars.ISO_8601;
+      use type League.Strings.Universal_String;
+
+      List : constant League.String_Vectors.Universal_String_Vector :=
+        Text.Split (' ');
+
+      Date   : League.Strings.Universal_String;
+      Year   : Year_Number;
+      Month  : Month_Number;
+      Day    : Day_Number;
+      Hour   : Hour_Number;
+      Minute : Minute_Number;
+      Second : Second_Number;
+   begin
+      --  Text example: Sunday, 06-Nov-94 08:49:37 GMT
+      Date := List.Element (2);
+
+      Year := Year_Number'Wide_Wide_Value
+        (Date.Tail_From (8).To_Wide_Wide_String);
+
+      if Year in 0 .. 49 then
+         Year := Year + 2000;
+      else
+         Year := Year + 1900;
+      end if;
+
+      Month := Month_Number
+        (Constants.Month_List.Index (Date.Slice (4, 6)) / 3);
+
+      Day := Day_Number'Wide_Wide_Value
+        (Date.Head_To (2).To_Wide_Wide_String);
+
+      Success := List.Element (4) = Constants.GMT;
+
+      Parse_Time (List.Element (3), Hour, Minute, Second);
+
+      Value := Create (Year, Month, Day, Hour, Minute, Second, 0);
+
+   exception
+      when Constraint_Error =>
+         Success := False;
+   end Parse_RFC_850_Date;
+   
+   
+   ----------------
+   -- Parse_Time --
+   ----------------
+
+   procedure Parse_Time
+     (Text   : League.Strings.Universal_String;
+      Hour   : out League.Calendars.ISO_8601.Hour_Number;
+      Minute : out League.Calendars.ISO_8601.Minute_Number;
+      Second : out League.Calendars.ISO_8601.Second_Number)
+   is
+      use League.Calendars.ISO_8601;
+
+      Time : constant League.String_Vectors.Universal_String_Vector
+        := Text.Split (':');
+   begin
+      Hour := Hour_Number'Wide_Wide_Value
+        (Time.Element (1).To_Wide_Wide_String);
+
+      Minute := Minute_Number'Wide_Wide_Value
+        (Time.Element (2).To_Wide_Wide_String);
+
+      Second := Second_Number'Wide_Wide_Value
+        (Time.Element (3).To_Wide_Wide_String);
+   end Parse_Time;
+   
+   
    ---------------
    -- To_String --
    ---------------
