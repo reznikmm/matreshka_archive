c42b979e2dfaadfd7739cada52ea002fdafcd876
Vadim Godunko <vgodunko@gmail.com>
2016-04-07 05:07:49 +0000

    Add sin and cos functions.
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@5574 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- source/web/tools/a2js/rtl/standard.js
+++ source/web/tools/a2js/rtl/standard.js
@@ -1,4 +1,49 @@
+//--------------------------------------------------------------------------//
+//                                                                          //
+//                            Matreshka Project                             //
+//                                                                          //
+//                               Web Framework                              //
+//                                                                          //
+//                        Runtime Library Component                         //
+//                                                                          //
+//--------------------------------------------------------------------------//
+//                                                                          //
+// Copyright © 2016, Vadim Godunko <vgodunko@gmail.com>                     //
+// All rights reserved.                                                     //
+//                                                                          //
+// Redistribution and use in source and binary forms, with or without       //
+// modification, are permitted provided that the following conditions       //
+// are met:                                                                 //
+//                                                                          //
+//  * Redistributions of source code must retain the above copyright        //
+//    notice, this list of conditions and the following disclaimer.         //
+//                                                                          //
+//  * Redistributions in binary form must reproduce the above copyright     //
+//    notice, this list of conditions and the following disclaimer in the   //
+//    documentation and/or other materials provided with the distribution.  //
+//                                                                          //
+//  * Neither the name of the Vadim Godunko, IE nor the names of its        //
+//    contributors may be used to endorse or promote products derived from  //
+//    this software without specific prior written permission.              //
+//                                                                          //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      //
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        //
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    //
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     //
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   //
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED //
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   //
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   //
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     //
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       //
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             //
+//                                                                          //
+//--------------------------------------------------------------------------//
+//  $Revision$ $Date$
+//--------------------------------------------------------------------------//
 //  This code is placeholder for Standard Ada package
+//--------------------------------------------------------------------------//
+
 define('standard', [], function(){
     var standard = {};
     var all_types = {};  //  Map from external_tag to tag(prototype)
@@ -68,7 +113,9 @@ define('standard', [], function(){
     //  Ada.Numerics.Generic_Complex_Elementary_Functions
     standard._generic_elementary_functions = function(){ return {
         arccos : Math.acos,
-        sqrt : Math.sqrt
+        cos    : Math.cos,
+        sin    : Math.sin,
+        sqrt   : Math.sqrt
     }};
 
     //  System.Address_To_Access_Conversions
