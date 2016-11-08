13e5044d2d0267b3fffbabca24f9f01ad6217789
Maxim Reznik <reznikmm@gmail.com>
2016-11-08 17:47:56 +0000

    WebAPI: add APIs for creating and navigating browsing contexts by name
    git-svn-id: file:///srv/svn/matreshka/trunk/matreshka@5659 661adc5f-87f8-0310-85d6-9a7b5f58d823

--- source/web/tools/a2js/webapi/html/webapi-html-windows.ads
+++ source/web/tools/a2js/webapi/html/webapi-html-windows.ads
@@ -62,4 +62,57 @@ package WebAPI.HTML.Windows is
              Convention => JavaScript_Property_Getter,
              Link_Name  => "document";
 
+   --  APIs for creating and navigating browsing contexts by name
+
+   not overriding function Open
+    (Self     : not null access WebAPI.HTML.Windows.Window;
+     URL      : WebAPI.DOM_String;
+     Name     : WebAPI.DOM_String;
+     Features : WebAPI.DOM_String)
+      return WebAPI.HTML.Windows.Window_Access is abstract
+        with Import     => True,
+              Convention => JavaScript_Method,
+              Link_Name  => "open";
+   --  Opens a window to show url (defaults to about:blank), and returns it.
+   --  The target argument gives the name of the new window. If a window
+   --  exists with that name already, it is reused. The features argument
+   --  can be used to influence the rendering of the new window.
+
+   not overriding function Get_Name
+    (Self : not null access Window)
+      return WebAPI.DOM_String is abstract
+        with Import     => True,
+             Convention => JavaScript_Property_Getter,
+             Link_Name  => "name";
+   --  Returns the name of the window. Can be set, to change the name.
+
+   not overriding procedure Set_Name
+    (Self  : not null access Window;
+     Value : WebAPI.DOM_String) is abstract
+        with Import     => True,
+             Convention => JavaScript_Property_Setter,
+             Link_Name  => "name";
+
+   not overriding procedure Close
+    (Self : not null access WebAPI.HTML.Windows.Window) is abstract
+         with Import     => True,
+              Convention => JavaScript_Method,
+              Link_Name  => "close";
+   --  Closes the window.
+
+   not overriding function Get_Closed
+    (Self : not null access Window)
+      return WebAPI.DOM_Boolean is abstract
+        with Import     => True,
+             Convention => JavaScript_Property_Getter,
+             Link_Name  => "closed";
+   --  Returns true if the window has been closed, false otherwise.
+
+   not overriding procedure Stop
+    (Self : not null access WebAPI.HTML.Windows.Window) is abstract
+         with Import     => True,
+              Convention => JavaScript_Method,
+              Link_Name  => "stop";
+   --  Cancels the document load.
+
 end WebAPI.HTML.Windows;
